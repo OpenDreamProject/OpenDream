dfdf05685edc7d6be4defaf7842578e58db16beb
wixoaGit <wixoag@gmail.com>
2020-12-22 12:26:55 -0500

    Cache resources server-side

--- OpenDreamServer/Resources/DreamResourceManager.cs
+++ OpenDreamServer/Resources/DreamResourceManager.cs
@@ -3,11 +3,13 @@ using OpenDreamShared.Net.Packets;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace OpenDreamServer.Resources {
     class DreamResourceManager {
         private string _rootPath;
+
+        private Dictionary<string, DreamResource> _resourceCache = new();
+        private object _resourceCacheLock = new object();
 
         public DreamResourceManager(string rootPath) {
             _rootPath = rootPath;
@@ -18,14 +20,27 @@ namespace OpenDreamServer.Resources {
         }
 
         public DreamResource LoadResource(string resourcePath) {
-            FileStream stream = new FileStream(Path.Combine(_rootPath, resourcePath), FileMode.Open);
-            byte[] resourceData = new byte[stream.Length];
-            int readBytes = 0;
+            DreamResource resource = null;
 
-            while (readBytes < stream.Length) readBytes += stream.Read(resourceData);
-            stream.Close();
+            lock (_resourceCacheLock) {
+                _resourceCache.TryGetValue(resourcePath, out resource);
+            }
 
-            return new DreamResource(resourcePath, resourceData);
+            if (resource == null) {
+                FileStream stream = new FileStream(Path.Combine(_rootPath, resourcePath), FileMode.Open);
+                byte[] resourceData = new byte[stream.Length];
+                int readBytes = 0;
+
+                while (readBytes < stream.Length) readBytes += stream.Read(resourceData);
+                stream.Close();
+
+                resource = new DreamResource(resourcePath, resourceData);
+                lock (_resourceCacheLock) {
+                    _resourceCache.Add(resourcePath, resource);
+                }
+            }
+
+            return resource;
         }
 
         public void HandleRequestResourcePacket(DreamConnection connection, PacketRequestResource pRequestResource) {

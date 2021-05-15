68847b9c455b84c1629ccadae6da4217410dd3e7
ike709 <ike709@users.noreply.github.com>
2021-05-14 21:18:15 -0500

    Makes copy_standard.bat recursive (#65)

--- DMCompiler/copy_standard.bat
+++ DMCompiler/copy_standard.bat
@@ -1,5 +1,5 @@
 @echo off
 if not exist bin\Debug\net5.0\DMStandard mkdir bin\Debug\net5.0\DMStandard
-xcopy DMStandard bin\Debug\net5.0\DMStandard /y
+xcopy DMStandard bin\Debug\net5.0\DMStandard /y /s /e
 if not exist bin\Release\net5.0\DMStandard mkdir bin\Release\net5.0\DMStandard
-xcopy DMStandard bin\Release\net5.0\DMStandard /y
+xcopy DMStandard bin\Release\net5.0\DMStandard /y /s /e

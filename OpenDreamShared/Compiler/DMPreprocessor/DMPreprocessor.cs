bdaa5b41cf0edb300e0cc36d3e2de0fdabdf373c
mumencoder <85894709+mumencoder@users.noreply.github.com>
2021-06-16 19:51:16 -0400

    Update OpenDreamShared/Compiler/DMPreprocessor/DMPreprocessor.cs
    Co-authored-by: wixoa <wixoag@gmail.com>

--- OpenDreamShared/Compiler/DMPreprocessor/DMPreprocessor.cs
+++ OpenDreamShared/Compiler/DMPreprocessor/DMPreprocessor.cs
@@ -24,10 +24,7 @@ namespace OpenDreamShared.Compiler.DMPreprocessor {
         }
 
         public void IncludeFile(string includePath, string file) {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                file = file.Replace('\\', '/');
-            }
+            file = file.Replace('\\', Path.DirectorySeparatorChar);
             string source = File.ReadAllText(Path.Combine(includePath, file));
             source = source.Replace("\r\n", "\n");
             source += '\n';

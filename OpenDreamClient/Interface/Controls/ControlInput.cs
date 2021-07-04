4ae4eead0012de6e9784c407c0dc6e49199c1441
wixoaGit <wixoag@gmail.com>
2021-07-03 21:00:19 -0400

    Input controls can now run commands

--- OpenDreamClient/Interface/Controls/ControlInput.cs
+++ OpenDreamClient/Interface/Controls/ControlInput.cs
@@ -1,13 +1,26 @@
 ﻿using OpenDreamShared.Interface;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace OpenDreamClient.Interface.Controls {
     class ControlInput : InterfaceControl {
+        private TextBox _textBox;
+
         public ControlInput(ControlDescriptor controlDescriptor, ControlWindow window) : base(controlDescriptor, window) { }
 
         protected override FrameworkElement CreateUIElement() {
-            return new TextBox();
+            _textBox = new TextBox();
+            _textBox.KeyDown += TextBox_KeyDown;
+
+            return _textBox;
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Return) {
+                Program.OpenDream.RunCommand(_textBox.Text);
+                _textBox.Clear();
+            }
         }
     }
 }

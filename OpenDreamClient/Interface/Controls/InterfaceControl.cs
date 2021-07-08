4e2bd99d87c9ce85077a38620bbe822468370cbf
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-07-08 11:32:52 +0200

    Some minor work on UI

--- OpenDreamClient/Interface/Controls/InterfaceControl.cs
+++ OpenDreamClient/Interface/Controls/InterfaceControl.cs
@@ -1,67 +1,51 @@
-﻿/*using OpenDreamShared.Interface;
+﻿using OpenDreamShared.Interface;
 using System;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controls;
+using Robust.Shared.Maths;
 
-namespace OpenDreamClient.Interface.Controls {
-    class InterfaceControl : InterfaceElement {
-        public FrameworkElement UIElement { get; private set; }
-        public bool IsDefault { get => _controlDescriptor.IsDefault; }
-        public System.Drawing.Size? Size { get => _controlDescriptor.Size; }
-        public System.Drawing.Point? Pos { get => _controlDescriptor.Pos; }
-        public System.Drawing.Point? Anchor1 { get => _controlDescriptor.Anchor1; }
-        public System.Drawing.Point? Anchor2 { get => _controlDescriptor.Anchor2; }
+namespace OpenDreamClient.Interface.Controls
+{
+    class InterfaceControl : Control, IInterfaceElement
+    {
+        private ControlDescriptor _controlDescriptor;
 
-        protected ControlDescriptor _controlDescriptor { get => _elementDescriptor as ControlDescriptor; }
-        protected ControlWindow _window;
-
-        public InterfaceControl(ControlDescriptor controlDescriptor, ControlWindow window) : base(controlDescriptor) {
-            _window = window;
-            UIElement = CreateUIElement();
-
-            UpdateElementDescriptor();
-        }
-
-        protected virtual FrameworkElement CreateUIElement() {
-            throw new NotImplementedException("Invalid InterfaceControl");
-        }
-
-        public override void UpdateElementDescriptor() {
-            System.Drawing.Point pos = _controlDescriptor.Pos.GetValueOrDefault();
-
-            Canvas.SetLeft(UIElement, pos.X);
-            Canvas.SetTop(UIElement, pos.Y);
-            if (UIElement is FrameworkElement frameworkElement) {
-                System.Drawing.Size size = _controlDescriptor.Size.GetValueOrDefault();
-
-                frameworkElement.Width = size.Width;
-                frameworkElement.Height = size.Height;
+        ElementDescriptor IInterfaceElement.ElementDescriptor
+        {
+            get => _controlDescriptor;
+            set
+            {
+                if (value is not ControlDescriptor controlDescriptor)
+                    throw new Exception($"ElementDescriptor must be able to be casted to {nameof(controlDescriptor)}!");
+                _controlDescriptor = controlDescriptor;
             }
-
-            _window?.UpdateAnchors();
-
-            if (_controlDescriptor.BackgroundColor.HasValue) {
-                System.Drawing.Color color = _controlDescriptor.BackgroundColor.Value;
-                Brush brush = new SolidColorBrush(Color.FromRgb(color.R, color.G, color.B));
-
-                switch (UIElement) {
-                    case Panel panel:
-                        panel.Background = brush;
-                        break;
-                    case Control control:
-                        control.Background = brush;
-                        break;
-                }
-            }
-
-            UIElement.Visibility = _controlDescriptor.IsVisible ? Visibility.Visible : Visibility.Hidden;
-            UIElement.IsEnabled = !_controlDescriptor.IsDisabled;
         }
 
-        public virtual void Output(string value, string data) {
+        public void SetAttribute(string name, object value)
+        {
+            throw new NotImplementedException();
+        }
 
+        protected override Vector2 MeasureOverride(Vector2 availableSize)
+        {
+            return new(_controlDescriptor.Size?.Width ?? 0, _controlDescriptor.Size?.Height ?? 0);
+        }
+
+        public void UpdateElementDescriptor()
+        {
+            LayoutContainer.SetAnchorLeft(this, _controlDescriptor.Anchor1?.X ?? 0);
+            LayoutContainer.SetAnchorTop(this, _controlDescriptor.Anchor1?.Y ?? 0);
+            LayoutContainer.SetAnchorRight(this, _controlDescriptor.Anchor2?.X ?? 0);
+            LayoutContainer.SetAnchorBottom(this, _controlDescriptor.Anchor2?.Y ?? 0);
+
+            // TODO ROBUST: Pos?
+
+            Visible = _controlDescriptor.IsVisible;
+        }
+
+        public void Shutdown()
+        {
+            Dispose();
         }
     }
 }
-*/

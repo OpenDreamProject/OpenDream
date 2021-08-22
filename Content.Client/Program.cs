83f022cc7e433c203f60e0af5864e580cd5077e5
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-08-22 10:44:02 +0200

    Specify startup branding in GameControllerOptions.

--- Content.Client/Program.cs
+++ Content.Client/Program.cs
@@ -1,11 +1,15 @@
 using Robust.Client;
+using Robust.Shared.Utility;
 
 namespace Content.Client {
     internal static class Program {
         public static void Main(string[] args) {
             ContentStart.StartLibrary(args, new GameControllerOptions() {
                 //Content.Shared makes use of lots of things that client sandboxing is not happy with
-                Sandboxing = false
+                Sandboxing = false,
+                DefaultWindowTitle = "OpenDream",
+                WindowIconSet = new ResourcePath("/OpenDream/Logo/Icon"),
+                SplashLogo = new ResourcePath("/OpenDream/Logo/logo.png"),
             });
         }
     }

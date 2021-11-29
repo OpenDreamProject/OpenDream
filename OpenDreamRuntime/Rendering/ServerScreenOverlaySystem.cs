af3b051aa222cdda866ec09c0dfa6ba877ef5e0f
wixoaGit <wixoag@gmail.com>
2021-11-28 22:18:28 -0500

    Include screen objects in PVS

--- OpenDreamRuntime/Rendering/ServerScreenOverlaySystem.cs
+++ OpenDreamRuntime/Rendering/ServerScreenOverlaySystem.cs
@@ -1,22 +1,45 @@
 ﻿using OpenDreamRuntime.Objects;
 using OpenDreamShared.Rendering;
+using Robust.Server.GameStates;
+using Robust.Server.Player;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
+using System.Collections.Generic;
 
 namespace OpenDreamRuntime.Rendering {
+
     class ServerScreenOverlaySystem : SharedScreenOverlaySystem {
         [Dependency] IAtomManager _atomManager = default!;
+
+        private Dictionary<IPlayerSession, HashSet<EntityUid>> _sessionToScreenObjects = new();
+
+        public override void Initialize() {
+            SubscribeLocalEvent<ExpandPvsEvent>(HandleExpandPvsEvent);
+        }
 
         public void AddScreenObject(DreamConnection connection, DreamObject screenObject) {
             EntityUid entityId = _atomManager.GetAtomEntity(screenObject).Uid;
 
+            if (!_sessionToScreenObjects.TryGetValue(connection.Session, out HashSet<EntityUid> objects)) {
+                objects = new HashSet<EntityUid>();
+                _sessionToScreenObjects.Add(connection.Session, objects);
+            }
+
+            objects.Add(entityId);
             RaiseNetworkEvent(new AddScreenObjectEvent(entityId), connection.Session.ConnectedClient);
         }
 
         public void RemoveScreenObject(DreamConnection connection, DreamObject screenObject) {
             EntityUid entityId = _atomManager.GetAtomEntity(screenObject).Uid;
 
+            _sessionToScreenObjects[connection.Session].Remove(entityId);
             RaiseNetworkEvent(new RemoveScreenObjectEvent(entityId), connection.Session.ConnectedClient);
+        }
+
+        private void HandleExpandPvsEvent(ref ExpandPvsEvent e) {
+            if (_sessionToScreenObjects.TryGetValue(e.Session, out HashSet<EntityUid> objects)) {
+                e.Entities.AddRange(objects);
+            }
         }
     }
 }

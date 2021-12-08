3931d5dfc7d83fc3752478c6c0e97f3bfd4cc5aa
ike709 <ike709@users.noreply.github.com>
2021-12-08 17:51:26 -0600

    Fixes an exception in ternary null handling (#513)
    * Fixes an exception in ternary null handling
    
    * Update DMCompiler/DM/Visitors/DMVisitorExpression.cs
    
    Co-authored-by: wixoa <wixoag@gmail.com>
    
    Co-authored-by: wixoa <wixoag@gmail.com>

--- DMCompiler/DM/Visitors/DMVisitorExpression.cs
+++ DMCompiler/DM/Visitors/DMVisitorExpression.cs
@@ -371,7 +371,7 @@ namespace DMCompiler.DM.Visitors {
         public void VisitTernary(DMASTTernary ternary) {
             var a = DMExpression.Create(_dmObject, _proc, ternary.A, _inferredPath);
             var b = DMExpression.Create(_dmObject, _proc, ternary.B, _inferredPath);
-            var c = DMExpression.Create(_dmObject, _proc, ternary.C, _inferredPath);
+            var c = DMExpression.Create(_dmObject, _proc, ternary.C ?? new DMASTConstantNull(ternary.Location), _inferredPath);
             Result = new Expressions.Ternary(ternary.Location, a, b, c);
         }
 

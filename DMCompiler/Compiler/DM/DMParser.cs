a6a9a832f06ad8e2f2056e30ff808152c7a4e41e
ike709 <ike709@github.com>
2021-12-17 00:06:30 -0600

    Two compiler opts

--- DMCompiler/Compiler/DM/DMParser.cs
+++ DMCompiler/Compiler/DM/DMParser.cs
@@ -531,21 +531,21 @@ namespace DMCompiler.Compiler.DM {
 
                 return new DMASTProcStatementExpression(loc, expression);
             } else {
-                DMASTProcStatement procStatement = ProcVarDeclaration();
-                if (procStatement == null) procStatement = Return();
-                if (procStatement == null) procStatement = Break();
-                if (procStatement == null) procStatement = Continue();
-                if (procStatement == null) procStatement = Goto();
-                if (procStatement == null) procStatement = Del();
-                if (procStatement == null) procStatement = Set();
-                if (procStatement == null) procStatement = Spawn();
-                if (procStatement == null) procStatement = If();
+                DMASTProcStatement procStatement = Return();
+                if (procStatement == null) procStatement = ProcVarDeclaration();
                 if (procStatement == null) procStatement = For();
+                if (procStatement == null) procStatement = Set();
+                if (procStatement == null) procStatement = Switch();
+                if (procStatement == null) procStatement = Continue();
+                if (procStatement == null) procStatement = Break();
+                if (procStatement == null) procStatement = Spawn();
                 if (procStatement == null) procStatement = While();
                 if (procStatement == null) procStatement = DoWhile();
-                if (procStatement == null) procStatement = Switch();
-                if (procStatement == null) procStatement = TryCatch();
                 if (procStatement == null) procStatement = Throw();
+                if (procStatement == null) procStatement = Del();
+                if (procStatement == null) procStatement = TryCatch();
+                if (procStatement == null) procStatement = Goto();
+                if (procStatement == null) procStatement = If();
 
                 if (procStatement != null) {
                     Whitespace();

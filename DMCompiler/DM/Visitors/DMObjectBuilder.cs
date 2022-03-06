c20c77eba731869b9e97c492ff19651ecb30abbc
ike709 <ike709@github.com>
2022-03-06 01:43:15 -0600

    Fixes setting a global to a global

--- DMCompiler/DM/Visitors/DMObjectBuilder.cs
+++ DMCompiler/DM/Visitors/DMObjectBuilder.cs
@@ -229,7 +229,7 @@ namespace DMCompiler.DM.Visitors {
                     variable.Value = new Expressions.Null(Location.Unknown);
                     EmitInitializationAssign(variable, expression);
                     break;
-
+                case Expressions.GlobalField: // Global set to another global
                 case Expressions.StringFormat:
                 case Expressions.ProcCall:
                     if (!variable.IsGlobal) throw new CompileErrorException(value.Location,$"Invalid initial value for \"{variable.Name}\"");

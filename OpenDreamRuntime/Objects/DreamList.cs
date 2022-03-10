a828fc0c0d138f50a60c320a103cd6fca38cdf51
ike709 <ike709@github.com>
2022-03-10 04:36:01 -0600

    Nuke list() allocs and fix del()

--- OpenDreamRuntime/Objects/DreamList.cs
+++ OpenDreamRuntime/Objects/DreamList.cs
@@ -14,20 +14,21 @@ namespace OpenDreamRuntime.Objects {
         internal event DreamListValueAssignedEventHandler? ValueAssigned;
         internal event DreamListBeforeValueRemovedEventHandler? BeforeValueRemoved;
 
-        private List<DreamValue> _values = new();
-        private Dictionary<DreamValue, DreamValue> _associativeValues = null;
+        private List<DreamValue> _values;
+        private Dictionary<DreamValue, DreamValue>? _associativeValues;
 
-        protected DreamList() : base(null)
+        protected DreamList(int size = 0) : base(null)
         {
+            _values = new List<DreamValue>(size);
             ObjectDefinition = _listDef ??= IoCManager.Resolve<IDreamManager>().ObjectTree.GetObjectDefinition(DreamPath.List);
         }
 
-        public static DreamList CreateUninitialized() {
-            return new DreamList();
+        public static DreamList CreateUninitialized(int size = 0) {
+            return new DreamList(size);
         }
 
-        public static DreamList Create() {
-            return new DreamList();
+        public static DreamList Create(int size = 0) {
+            return new DreamList(size);
         }
 
         public static DreamList Create(IEnumerable<object> collection) {
@@ -45,16 +46,17 @@ namespace OpenDreamRuntime.Objects {
         }
 
         public DreamList CreateCopy(int start = 1, int end = 0) {
-            DreamList copy = Create();
 
             if (start == 0) ++start; //start being 0 and start being 1 are equivalent
             if (end > _values.Count + 1) throw new Exception("list index out of bounds");
             if (end == 0) end = _values.Count + 1;
 
+            DreamList copy = Create(end);
+
             for (int i = start; i < end; i++) {
                 DreamValue value = _values[i - 1];
 
-                copy._values.Add(value);
+                copy._values[i - 1] = value;
                 if (ContainsKey(value)) {
                     copy.SetValue(value, _associativeValues[value]);
                 }

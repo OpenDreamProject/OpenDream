c3bd58b6c2ea90933f59e459edb7297805f269fc
ike709 <ike709@users.noreply.github.com>
2022-05-22 23:19:55 -0500

    Declare `type` on `/list` (#701)
    * Declare `type` on `/list`
    
    * getter
    
    * switch
    
    Co-authored-by: ike709 <ike709@github.com>

--- OpenDreamRuntime/Objects/MetaObjects/DreamMetaObjectList.cs
+++ OpenDreamRuntime/Objects/MetaObjects/DreamMetaObjectList.cs
@@ -1,4 +1,5 @@
 ﻿using OpenDreamRuntime.Procs;
+using OpenDreamShared.Dream;
 
 namespace OpenDreamRuntime.Objects.MetaObjects {
     class DreamMetaObjectList : DreamMetaObjectRoot {
@@ -21,13 +22,19 @@ namespace OpenDreamRuntime.Objects.MetaObjects {
             }
         }
 
-        public override DreamValue OnVariableGet(DreamObject dreamObject, string variableName, DreamValue variableValue) {
-            if (variableName == "len") {
-                DreamList list = (DreamList)dreamObject;
-
-                return new DreamValue(list.GetLength());
-            } else {
-                return base.OnVariableGet(dreamObject, variableName, variableValue);
+        public override DreamValue OnVariableGet(DreamObject dreamObject, string variableName, DreamValue variableValue)
+        {
+            switch (variableName)
+            {
+                case "len":
+                {
+                    DreamList list = (DreamList)dreamObject;
+                    return new DreamValue(list.GetLength());
+                }
+                case "type":
+                    return new DreamValue(DreamPath.List);
+                default:
+                    return base.OnVariableGet(dreamObject, variableName, variableValue);
             }
         }
 

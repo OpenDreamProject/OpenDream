a87aed0bf31ac983fdbe85a26fd231ca0954a0e9
ike709 <ike709@users.noreply.github.com>
2022-06-11 19:38:36 -0500

    Update DMCompiler/Compiler/DM/DMParser.cs

--- DMCompiler/Compiler/DM/DMParser.cs
+++ DMCompiler/Compiler/DM/DMParser.cs
@@ -345,10 +345,6 @@ namespace DMCompiler.Compiler.DM {
                 if (sizes.Count > 0)
                 {
                     DMASTExpression[] expressions = sizes.ToArray()
-                    for (var i = 0; i < sizes.Count; i++)
-                    {
-                        expressions[i] = sizes[i];
-                    }
                     implied_value = new DMASTNewMultidimensionalList(loc, expressions);
                 }
 

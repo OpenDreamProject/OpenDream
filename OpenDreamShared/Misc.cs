adfd148cf6d9cad3cd0cad0b91b85dff54f7718e
Gary Lafortune <mom-puter@hotmail.com>
2022-09-02 20:13:56 -0500

    Adds a basic RAII-like defer pattern in ODS
    I don't entirely trust this pattern, nor can I really commend its beauty, but we are in desperate need of a method to defer things throughout DMParser. Many functions have clunky syntax or erratic behaviour as a result of not always doing a task on all possible control paths. I've seen a couple TODOs about it as well so, here ya go.

---
+++ OpenDreamShared/Misc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDreamShared
+{
+    /// <summary>
+    /// TODO: Try to think of a better name for this (dummy) class.
+    /// </summary>
+    public static class Misc
+    {
+        /// <summary>
+        /// Defers calling the given action until the block is completed. <br/>
+        /// To use this properly, you must cast a magic spell against the GC, in this specific way: <br/>
+        /// <see langword="using"/> <see langword="var"/> _ = Defer(b =&gt; bluh(b), maybe_captured_parameter); <br/>
+        /// If everything goes well (!!), this MUST be called before the block that invokes it returns.
+        /// </summary>
+        public static DeferDisposable<T> Defer<T>(Action<T> action, T param1) => new DeferDisposable<T>(action, param1);
+
+        /// <inheritdoc cref="Defer{T}(Action{T}, T)"/>
+        public static DeferDisposableVoid Defer(Action action) => new DeferDisposableVoid(action);
+
+        public readonly struct DeferDisposable<T1> : IDisposable
+        {
+            readonly Action<T1> _action;
+            readonly T1 _param1;
+            public DeferDisposable(Action<T1> action, T1 param1)
+            {
+                _action = action;
+                _param1 = param1;
+            }
+            public void Dispose() => _action.Invoke(_param1);
+        }
+        public readonly struct DeferDisposableVoid: IDisposable
+        {
+            readonly Action _action;
+            public DeferDisposableVoid(Action action)
+            {
+                _action = action;
+            }
+            public void Dispose() => _action.Invoke();
+        }
+    }
+}

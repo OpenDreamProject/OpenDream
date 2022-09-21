02ae6f17e5dd56ab8264936bab45a138f8c3e909
Penelope Haze <110272328+out-of-phaze@users.noreply.github.com>
2022-09-21 12:28:34 -0500

    Improve error catching for embedded expressions (#801)
    * Emit errors on invalid embedded expressions
    
    Fix empty/whitespace embedded expressions
    
    Fix errors in embedded expressions not propagating
    
    * Add unit tests for bad embedded expressions
    
    Fix embedded expression unit tests
    
    Fix embedded expression hole test

--- DMCompiler/Compiler/DM/DMParser.cs
+++ DMCompiler/Compiler/DM/DMParser.cs
@@ -2040,7 +2040,7 @@ namespace DMCompiler.Compiler.DM {
                                     insideBrackets.Remove(insideBrackets.Length - 1, 1); //Remove the ending bracket
 
                                     string insideBracketsText = insideBrackets?.ToString();
-                                    if (insideBracketsText != String.Empty) {
+                                    if (!String.IsNullOrWhiteSpace(insideBracketsText)) {
                                         DMPreprocessorLexer preprocLexer = new DMPreprocessorLexer(null, constantToken.Location.SourceFile, insideBracketsText);
                                         List<Token> preprocTokens = new();
                                         Token preprocToken;
@@ -2058,10 +2058,11 @@ namespace DMCompiler.Compiler.DM {
                                             expressionParser.Whitespace(true);
                                             expression = expressionParser.Expression();
                                             if (expression == null) Error("Expected an expression");
-                                        } catch (CompileErrorException e) {
-                                            Errors.Add(e.Error);
+                                            if (expressionParser.Current().Type != TokenType.EndOfFile) Error("Expected end of embedded statement");
+                                        } catch (CompileErrorException) {
                                         }
 
+                                        if (expressionParser.Errors.Count > 0) Errors.AddRange(expressionParser.Errors);
                                         if (expressionParser.Warnings.Count > 0) Warnings.AddRange(expressionParser.Warnings);
                                         interpolationValues.Add(expression);
                                     } else {

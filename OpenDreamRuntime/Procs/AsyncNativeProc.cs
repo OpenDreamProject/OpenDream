961ffaba054031d9a3fe2a149a6830ebcbfff17b
Edgelord_exe <magicznyszop@gmail.com>
2022-09-24 19:08:42 +0200

    async native?

--- OpenDreamRuntime/Procs/AsyncNativeProc.cs
+++ OpenDreamRuntime/Procs/AsyncNativeProc.cs
@@ -34,6 +34,16 @@ namespace OpenDreamRuntime.Procs {
                 Src = src;
                 Usr = usr;
                 Arguments = arguments;
+
+                if(Src is not null){
+                    Src.IncrementRefCount();
+                }
+                if(Usr is not null){
+                    Usr.IncrementRefCount();
+                }
+                foreach(var arg in arguments.GetAllArguments()){
+                    arg.IncrementDreamObjectRefCount();
+                }
             }
 
             // Used to avoid reentrant resumptions in our proc

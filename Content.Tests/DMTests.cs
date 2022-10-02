f80ba392572ea66b16d785ce132dbf5a0d016097
ike709 <ike709@github.com>
2022-10-02 14:30:48 -0700

    Check If Broken Tests Pass

--- Content.Tests/DMTests.cs
+++ Content.Tests/DMTests.cs
@@ -64,27 +64,66 @@ namespace Content.Tests
             try {
                 string compiledFile = Compile(Path.Join(initialDirectory, "Tests", sourceFile));
                 if (testFlags.HasFlag(DMTestFlags.CompileError)) {
-                    Assert.IsNull(compiledFile, $"Expected an error during DM compilation");
+                    Assert.IsNull(compiledFile, $"Expected an error during DM compilation of '{sourceFile}'");
                     Cleanup(compiledFile);
                     return;
                 }
 
-                Assert.IsTrue(compiledFile is not null && File.Exists(compiledFile), $"Failed to compile DM source file");
+                Assert.IsTrue(compiledFile is not null && File.Exists(compiledFile), $"Failed to compile DM source file '{sourceFile}'");
                 Assert.IsTrue(_dreamMan.LoadJson(compiledFile), $"Failed to load {compiledFile}");
 
                 (bool successfulRun, DreamValue returned, Exception? exception) = RunTest();
                 if (testFlags.HasFlag(DMTestFlags.RuntimeError)) {
-                    Assert.IsFalse(successfulRun, "A DM runtime exception was expected");
+                    Assert.IsFalse(successfulRun, $"A DM runtime exception was expected in '{sourceFile}'");
                 } else {
                     if (exception != null)
-                        Assert.IsTrue(successfulRun, $"A DM runtime exception was thrown: \"{exception.Message}\"");
+                        Assert.IsTrue(successfulRun, $"A DM runtime exception was thrown in '{sourceFile}': \"{exception.Message}\"");
                     else
-                        Assert.IsTrue(successfulRun, "A DM runtime exception was thrown, and its message could not be recovered!");
+                        Assert.IsTrue(successfulRun, $"A DM runtime exception was thrown in '{sourceFile}', and its message could not be recovered!");
                 }
 
                 if (testFlags.HasFlag(DMTestFlags.ReturnTrue)) {
                     returned.TryGetValueAsInteger(out int returnInt);
-                    Assert.IsTrue(returnInt != 0, "Test was expected to return TRUE");
+                    Assert.IsTrue(returnInt != 0, $"Test '{sourceFile}' was expected to return TRUE");
+                }
+
+                Cleanup(compiledFile);
+            } finally {
+                // Restore the original CurrentDirectory, since loading a compiled JSON changes it.
+                Directory.SetCurrentDirectory(initialDirectory);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(GetBrokenTests))]
+        public void TestBrokenFiles(string sourceFile, DMTestFlags testFlags)
+        {
+            string initialDirectory = Directory.GetCurrentDirectory();
+            try {
+                string compiledFile = Compile(Path.Join(initialDirectory, "Broken Tests", sourceFile));
+                if (testFlags.HasFlag(DMTestFlags.CompileError)) {
+                    Assert.IsNotNull(compiledFile, $"Broken test '{sourceFile}' appears to be fixed, please move it to 'Tests'");
+                    Cleanup(compiledFile);
+                    return;
+                }
+
+                if (compiledFile is null || !File.Exists(compiledFile) || !_dreamMan.LoadJson(compiledFile)) {
+                    Cleanup(compiledFile);
+                    return;
+                }
+
+                (bool successfulRun, DreamValue returned, Exception? exception) = RunTest();
+                if (testFlags.HasFlag(DMTestFlags.RuntimeError)) {
+                    Assert.IsTrue(successfulRun, $"Broken test '{sourceFile}' appears to be fixed, please move it to 'Tests'");
+                } else {
+                    if (exception != null)
+                        Assert.IsFalse(successfulRun, $"Broken test '{sourceFile}' appears to be fixed, please move it to 'Tests'");
+                    else
+                        Assert.IsFalse(successfulRun, $"Broken test '{sourceFile}' appears to be fixed, please move it to 'Tests'");
+                }
+
+                if (testFlags.HasFlag(DMTestFlags.ReturnTrue)) {
+                    returned.TryGetValueAsInteger(out int returnInt);
+                    Assert.IsTrue(returnInt == 0, $"Broken test '{sourceFile}' appears to be fixed, please move it to 'Tests'");
                 }
 
                 Cleanup(compiledFile);
@@ -114,10 +153,31 @@ namespace Content.Tests
 
         private static IEnumerable<object[]> GetTests()
         {
-            Directory.SetCurrentDirectory(TestProject);
+            if (!Directory.GetCurrentDirectory().EndsWith(TestProject)) {
+                Directory.SetCurrentDirectory(TestProject);
+            }
 
             foreach (string sourceFile in Directory.GetFiles("Tests", "*.dm", SearchOption.AllDirectories)) {
+                if(!File.Exists(sourceFile)) continue;
                 string sourceFile2 = sourceFile.Substring("Tests/".Length);
+                DMTestFlags testFlags = GetDMTestFlags(sourceFile);
+                if (testFlags.HasFlag(DMTestFlags.Ignore))
+                    continue;
+
+                yield return new object[] {
+                    sourceFile2,
+                    testFlags
+                };
+            }
+        }
+
+        private static IEnumerable<object[]> GetBrokenTests() {
+            if (!Directory.GetCurrentDirectory().EndsWith(TestProject)) {
+                Directory.SetCurrentDirectory(TestProject);
+            }
+
+            foreach (string sourceFile in Directory.GetFiles("Broken Tests", "*.dm", SearchOption.AllDirectories)) {
+                string sourceFile2 = sourceFile.Substring("Broken Tests/".Length);
                 DMTestFlags testFlags = GetDMTestFlags(sourceFile);
                 if (testFlags.HasFlag(DMTestFlags.Ignore))
                     continue;

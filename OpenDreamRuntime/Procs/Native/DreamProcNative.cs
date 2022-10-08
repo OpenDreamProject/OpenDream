f8f605d422fe34008a4df62d74b4aabf795c7e2d
ike709 <ike709@github.com>
2022-10-08 11:47:52 -0700

    Implement `copytext_char()`

--- OpenDreamRuntime/Procs/Native/DreamProcNative.cs
+++ OpenDreamRuntime/Procs/Native/DreamProcNative.cs
@@ -18,6 +18,7 @@ namespace OpenDreamRuntime.Procs.Native {
             objectTree.SetGlobalNativeProc(DreamProcNativeRoot.NativeProc_clamp);
             objectTree.SetGlobalNativeProc(DreamProcNativeRoot.NativeProc_cmptext);
             objectTree.SetGlobalNativeProc(DreamProcNativeRoot.NativeProc_copytext);
+            objectTree.SetGlobalNativeProc(DreamProcNativeRoot.NativeProc_copytext_char);
             objectTree.SetGlobalNativeProc(DreamProcNativeRoot.NativeProc_cos);
             objectTree.SetGlobalNativeProc(DreamProcNativeRoot.NativeProc_CRASH);
             objectTree.SetGlobalNativeProc(DreamProcNativeRoot.NativeProc_fcopy);

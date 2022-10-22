179e0ffdec84b9ba9082266696bb0b078ef12d6b
wixoaGit <wixoag@gmail.com>
2022-10-22 17:45:33 -0400

    Add support for DreamResource in `ref()`

--- OpenDreamRuntime/IDreamManager.cs
+++ OpenDreamRuntime/IDreamManager.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using OpenDreamRuntime.Objects;
+﻿using OpenDreamRuntime.Objects;
 using Robust.Server.Player;
 
 namespace OpenDreamRuntime {
@@ -34,91 +33,18 @@ namespace OpenDreamRuntime {
 
         public void WriteWorldLog(string message, LogLevel level, string sawmill = "world.log");
 
-        public virtual string CreateRef(DreamValue value) {
-            RefType refType;
-            int idx;
-
-            if (value.TryGetValueAsDreamObject(out var refObject)) {
-                if (refObject == null) {
-                    refType = RefType.Null;
-                    idx = 0;
-                } else {
-                    if(refObject.Deleted) {
-                        // i dont believe this will **ever** be called, but just to be sure, funky errors /might/ appear in the future if someone does a fucky wucky and calls this on a deleted object.
-                        throw new Exception("Cannot create reference ID for an object that is deleted");
-                    }
-
-                    refType = RefType.DreamObject;
-                    if (!ReferenceIDs.TryGetValue(refObject, out idx)) {
-                        idx = ReferenceIDs.Count;
-                        ReferenceIDs.Add(refObject, idx);
-                    }
-                }
-            } else if (value.TryGetValueAsString(out var refStr)) {
-                refType = RefType.String;
-                idx = ObjectTree.Strings.IndexOf(refStr);
-
-                if (idx == -1) {
-                    ObjectTree.Strings.Add(refStr);
-                    idx = ObjectTree.Strings.Count - 1;
-                }
-            } else if (value.TryGetValueAsPath(out var refPath)) {
-                var treeEntry = ObjectTree.GetTreeEntry(refPath);
-
-                refType = RefType.DreamPath;
-                idx = treeEntry.Id;
-            } else {
-                throw new NotImplementedException($"Ref for {value} is unimplemented");
-            }
-
-            // The first digit is the type, i.e. 1 for objects and 2 for strings
-            return $"{(int) refType}{idx}";
-        }
-
-        public DreamValue LocateRef(string refString) {
-            if (!int.TryParse(refString, out var refId)) {
-                // If the ref is not an integer, it may be a tag
-                if (Tags.TryGetValue(refString, out var tagList)) {
-                    return new DreamValue(tagList.First());
-                }
-
-                return DreamValue.Null;
-            }
-
-            // The first digit is the type
-            var typeId = (RefType) int.Parse(refString.Substring(0, 1));
-            var untypedRefString = refString.Substring(1); // The ref minus its ref type prefix
-            refId = int.Parse(untypedRefString);
-
-            switch (typeId) {
-                case RefType.Null:
-                    return DreamValue.Null;;
-                case RefType.DreamObject:
-                    foreach (KeyValuePair<DreamObject, int> referenceIdPair in ReferenceIDs) {
-                        if (referenceIdPair.Value == refId) return new DreamValue(referenceIdPair.Key);
-                    }
-
-                    return DreamValue.Null;
-                case RefType.String:
-                    return ObjectTree.Strings.Count > refId
-                        ? new DreamValue(ObjectTree.Strings[refId])
-                        : DreamValue.Null;
-                case RefType.DreamPath:
-                    return ObjectTree.Types.Length > refId
-                        ? new DreamValue(ObjectTree.Types[refId].Path)
-                        : DreamValue.Null;
-                default:
-                    throw new Exception($"Invalid reference type for ref {refString}");
-            }
-        }
+        public string CreateRef(DreamValue value);
+        public DreamValue LocateRef(string refString);
 
         IEnumerable<DreamConnection> Connections { get; }
     }
 
+    // TODO: Could probably use DreamValueType instead
     public enum RefType : int {
         Null = 0,
         DreamObject = 1,
         String = 2,
-        DreamPath = 3
+        DreamPath = 3,
+        DreamResource = 4
     }
 }

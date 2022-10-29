a679dbbb1c6bc87a29fd05496dfa7ce55a5bcbe2
amylizzle <amylizzlep@gmail.com>
2022-10-29 20:39:12 +0100

    Tmp fix for no interface specified. See #851

--- DMCompiler/DMCompiler.cs
+++ DMCompiler/DMCompiler.cs
@@ -223,7 +223,7 @@ namespace DMCompiler {
             DreamCompiledJson compiledDream = new DreamCompiledJson();
             compiledDream.Strings = DMObjectTree.StringTable;
             compiledDream.Maps = maps;
-            compiledDream.Interface = Path.GetRelativePath(Path.GetDirectoryName(outputFile), interfaceFile);
+            compiledDream.Interface = string.IsNullOrEmpty(interfaceFile) ? "" : Path.GetRelativePath(Path.GetDirectoryName(outputFile), interfaceFile);
             var jsonRep = DMObjectTree.CreateJsonRepresentation();
             compiledDream.Types = jsonRep.Item1;
             compiledDream.Procs = jsonRep.Item2;

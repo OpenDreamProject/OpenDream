70315640c6cae5f87420233df4f802121c572cb1
Amy <amylizzle@users.noreply.github.com>
2022-10-30 20:13:23 +0000

    Update OpenDreamClient/Rendering/ClientAppearanceSystem.cs
    Co-authored-by: wixoa <wixoag@gmail.com>

--- OpenDreamClient/Rendering/ClientAppearanceSystem.cs
+++ OpenDreamClient/Rendering/ClientAppearanceSystem.cs
@@ -4,6 +4,7 @@ using Robust.Shared.Map;
 using SharedAppearanceSystem = OpenDreamShared.Rendering.SharedAppearanceSystem;
 using Robust.Client.Graphics;
 using Robust.Shared.Prototypes;
+
 namespace OpenDreamClient.Rendering {
     sealed class ClientAppearanceSystem : SharedAppearanceSystem {
         private Dictionary<uint, IconAppearance> _appearances = new();

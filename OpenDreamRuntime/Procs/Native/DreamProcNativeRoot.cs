07042c71fca081a23ab2ea390e83118f8c5011f3
ike709 <ike709@users.noreply.github.com>
2022-11-12 19:35:24 -0700

    Fixes `typesof()` for non-paths (#813)
    * Fixes typesof() for non-paths
    
    * fix whitespace
    
    * typesof() dreamobject support
    
    * strings too
    
    * ignore lists
    
    * Fix proc strings
    
    Credit to out-of-phaze for this code
    
    Co-authored-by: ike709 <ike709@github.com>

--- OpenDreamRuntime/Procs/Native/DreamProcNativeRoot.cs
+++ OpenDreamRuntime/Procs/Native/DreamProcNativeRoot.cs
@@ -1927,7 +1927,28 @@ namespace OpenDreamRuntime.Procs.Native {
             DreamList list = DreamList.Create();
 
             foreach (DreamValue type in arguments.GetAllArguments()) {
-                DreamPath typePath = type.GetValueAsPath();
+                if (!type.TryGetValueAsPath(out var typePath)) {
+                    if (!type.TryGetValueAsDreamObject(out var typeObj) || typeObj?.ObjectDefinition is null || typeObj is DreamList) {
+                        if (type.TryGetValueAsString(out var typeString)) {
+                            var stringToPath = new DreamPath(typeString);
+                            if (stringToPath.LastElement == "proc") {
+                                DreamPath objectPath = stringToPath.AddToPath("..");
+                                if (!DreamManager.ObjectTree.HasTreeEntry(objectPath))
+                                {
+                                    continue;
+                                }
+                            }
+                            else if (!DreamManager.ObjectTree.HasTreeEntry(stringToPath)) {
+                                continue;
+                            }
+                            typePath = stringToPath;
+                        } else {
+                            continue;
+                        }
+                    } else {
+                        typePath = typeObj.ObjectDefinition.Type;
+                    }
+                }
 
                 if (typePath.LastElement == "proc") {
                     DreamPath objectTypePath = typePath.AddToPath("..");

db804557c7668b5bbcbff2f6e183c7c0d6197daf
Paul Ritter <ritter.paul1@googlemail.com>
2023-03-09 05:00:38 +0100

    implements winclone (#1041)
    * winclone basic logic
    
    * final logic
    
    * aligning controlId parsing with spec
    
    * sorry but the space bothered me
    
    * fix getting initial descriptor
    
    * partially address reviews
    
    * makes descriptors immutable
    
    * Address review & make things functional
    
    ---------
    
    Co-authored-by: wixoaGit <wixoag@gmail.com>

--- OpenDreamClient/Interface/Descriptors/MenuDescriptors.cs
+++ OpenDreamClient/Interface/Descriptors/MenuDescriptors.cs
@@ -1,30 +1,58 @@
-﻿using Robust.Shared.Serialization.Manager;
+﻿using JetBrains.Annotations;
+using Robust.Shared.Serialization.Manager;
 using Robust.Shared.Serialization.Markdown.Mapping;
 
 namespace OpenDreamClient.Interface.Descriptors;
 
 public sealed class MenuDescriptor : ElementDescriptor {
-    public readonly List<MenuElementDescriptor> Elements = new();
+    private readonly List<MenuElementDescriptor> _elements = new();
+    public IReadOnlyList<MenuElementDescriptor> Elements => _elements;
 
     public MenuDescriptor(string name) {
+        Type = "MENU";
         Name = name;
+    }
+
+    [UsedImplicitly]
+    public MenuDescriptor() {
+
     }
 
     public override MenuElementDescriptor CreateChildDescriptor(ISerializationManager serializationManager, MappingDataNode attributes) {
         var menuElement = serializationManager.Read<MenuElementDescriptor>(attributes);
 
-        Elements.Add(menuElement);
+        _elements.Add(menuElement);
         return menuElement;
+    }
+
+    public override ElementDescriptor CreateCopy(ISerializationManager serializationManager, string name) {
+        var copy = serializationManager.CreateCopy(this);
+
+        copy._name = name;
+        return copy;
     }
 }
 
 public sealed class MenuElementDescriptor : ElementDescriptor {
+    private string _category;
+
     [DataField("command")]
-    public string Command;
+    public string Command { get; init; }
+
     [DataField("category")]
-    public string Category;
+    public string Category {
+        get => _category;
+        init => _category = value;
+    }
+
     [DataField("can-check")]
-    public bool CanCheck;
+    public bool CanCheck { get; init; }
+
+    public MenuElementDescriptor WithCategory(ISerializationManager serialization, string category) {
+        var copy = serialization.CreateCopy(this);
+        copy._category = category;
+        return copy;
+    }
 
     // Menu elements can have other menu elements as children
     public override MenuElementDescriptor CreateChildDescriptor(ISerializationManager serializationManager, MappingDataNode attributes) {
---
+++ OpenDreamShared/Network/Messages/MsgWinClone.cs
@@ -0,0 +1,22 @@
+﻿using Lidgren.Network;
+using Robust.Shared.Network;
+using Robust.Shared.Serialization;
+
+namespace OpenDreamShared.Network.Messages;
+
+public sealed class MsgWinClone : NetMessage {
+    public override MsgGroups MsgGroup => MsgGroups.EntityEvent;
+
+    public string ControlId;
+    public string CloneId;
+
+    public override void ReadFromBuffer(NetIncomingMessage buffer, IRobustSerializer serializer) {
+        ControlId = buffer.ReadString();
+        CloneId = buffer.ReadString();
+    }
+
+    public override void WriteToBuffer(NetOutgoingMessage buffer, IRobustSerializer serializer) {
+        buffer.Write(ControlId);
+        buffer.Write(CloneId);
+    }
+}

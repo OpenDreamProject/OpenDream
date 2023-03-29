edf54492990893bfe097fc0196f748b0f8f5e407
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2023-03-28 17:54:15 -0700

    Update OpenDreamClient/EntryPoint.cs
    Co-authored-by: ZeWaka <zewakagamer@gmail.com>

--- OpenDreamClient/EntryPoint.cs
+++ OpenDreamClient/EntryPoint.cs
@@ -21,7 +21,7 @@ namespace OpenDreamClient {
         private readonly IDreamResourceManager _dreamResource = default!;
 
         private const string UserAgent =
-            "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.2; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729) ";
+            "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.2; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)";
 
         public override void PreInit()
         {

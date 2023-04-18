d0d35e32427297c078999e08ed5b0b863edcace9
wixoaGit <wixoag@gmail.com>
2023-04-18 11:03:00 -0400

    Add line numbers to DM stack traces

--- OpenDreamRuntime/Procs/DMProc.cs
+++ OpenDreamRuntime/Procs/DMProc.cs
@@ -324,6 +324,8 @@ namespace OpenDreamRuntime.Procs {
             }
 
             builder.Append(Proc.Name);
+            builder.Append(':');
+            builder.Append(CurrentLine);
         }
 
         public void Jump(int position) {

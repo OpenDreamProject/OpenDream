e0e8330ce7e77ebd12a678d0cb3a9d07d75459fe
wixoaGit <wixoag@gmail.com>
2023-07-11 15:02:12 -0400

    Don't return from view() calculation if there is no eye No reason it can't still work if there's no tile in the center

--- OpenDreamShared/Dream/ViewAlgorithm.cs
+++ OpenDreamShared/Dream/ViewAlgorithm.cs
@@ -61,8 +61,7 @@ public static class ViewAlgorithm {
             if (tile.DeltaX == 0 && tile.DeltaY == 0)
                 eye = tile;
         }
-        if(eye == null)
-            return;
+
         // TODO: Lummox mentions an optimization in step 3 and 4. Probably worthwhile.
 
         // Step 3, Diagonal shadow loop
@@ -101,7 +100,8 @@ public static class ViewAlgorithm {
         }
 
         // Step 5
-        eye!.Vis = 1;
+        if (eye != null)
+            eye.Vis = 1;
 
         // TODO: Step 6, Light loop
 

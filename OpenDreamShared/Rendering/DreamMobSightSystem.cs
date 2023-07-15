a9fed886e7102a07e0d56c79b4c951d9da66a133
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2023-07-15 15:32:46 +0200

    Get rid of last GetComponenStates
    Needed to update Robust

---
+++ OpenDreamShared/Rendering/DreamMobSightSystem.cs
@@ -0,0 +1,25 @@
+﻿using Robust.Shared.GameObjects;
+using Robust.Shared.GameStates;
+
+namespace OpenDreamShared.Rendering;
+
+public sealed class DreamMobSightSystem : EntitySystem {
+    public override void Initialize() {
+        SubscribeLocalEvent<DreamMobSightComponent, ComponentGetState>(GetComponentState);
+        SubscribeLocalEvent<DreamMobSightComponent, ComponentHandleState>(HandleComponentState);
+    }
+
+    private static void GetComponentState(EntityUid uid, DreamMobSightComponent component, ref ComponentGetState args) {
+        args.State = new DreamMobSightComponentState(
+            component.SeeInvisibility, component.Sight);
+    }
+
+    private static void HandleComponentState(EntityUid uid, DreamMobSightComponent component, ref ComponentHandleState args) {
+        DreamMobSightComponentState? state = (DreamMobSightComponentState?)args.Current;
+        if (state == null)
+            return;
+
+        component.SeeInvisibility = state.SeeInvisibility;
+        component.Sight = state.Sight;
+    }
+}

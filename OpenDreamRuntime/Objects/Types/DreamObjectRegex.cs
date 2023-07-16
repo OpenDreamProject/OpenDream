2a8e5da139cd36651a00e5a5a0c3b01e24083869
Redmoogle <dakotamew@gmail.com>
2023-07-15 21:19:28 -0400

    Simplify regex replacement

--- OpenDreamRuntime/Objects/Types/DreamObjectRegex.cs
+++ OpenDreamRuntime/Objects/Types/DreamObjectRegex.cs
@@ -25,9 +25,9 @@ public sealed class DreamObjectRegex : DreamObject {
         } else if (pattern.TryGetValueAsString(out var patternString)) {
             RegexOptions options = RegexOptions.None;
             if (flags.TryGetValueAsString(out var flagsString)) {
-                if (flagsString.Contains("i")) options |= RegexOptions.IgnoreCase;
-                if (flagsString.Contains("m")) options |= RegexOptions.Multiline;
-                if (flagsString.Contains("g")) IsGlobal = true;
+                if (flagsString.Contains('i')) options |= RegexOptions.IgnoreCase;
+                if (flagsString.Contains('m')) options |= RegexOptions.Multiline;
+                if (flagsString.Contains('g')) IsGlobal = true;
             }
 
             ReplaceEscapeCodes(ref patternString);
@@ -41,30 +41,11 @@ public sealed class DreamObjectRegex : DreamObject {
     /// Replaces escape codes that BYOND recognizes but C# doesn't
     /// </summary>
     private static void ReplaceEscapeCodes(ref string regex) {
-        // TODO Make this more Robust(TM)
 
-        var anyLetterIdx = regex.IndexOf("\\l", StringComparison.InvariantCulture); // From the ref: \l = Any letter A through Z, case-insensitive
-        while (anyLetterIdx >= 0) {
-            if (anyLetterIdx == 0 || regex[anyLetterIdx - 1] != '\\') { // TODO Need to make this handle an arbitrary number of escape chars
-                regex = regex.Remove(anyLetterIdx, 2).Insert(anyLetterIdx, "[A-Za-z]");
-            }
-
-            var nextIdx = anyLetterIdx + 1;
-            if(nextIdx >= regex.Length) break;
-
-            anyLetterIdx = regex.IndexOf("\\l", nextIdx, StringComparison.InvariantCulture);
-        }
-
-        var anyButLetterIdx = regex.IndexOf("\\L", StringComparison.InvariantCulture); // From the ref: \L = Any character except a letter or line break
-        while (anyButLetterIdx >= 0) {
-            if (anyButLetterIdx == 0 || regex[anyButLetterIdx - 1] != '\\') { // TODO Need to make this handle an arbitrary number of escape chars
-                regex = regex.Remove(anyButLetterIdx, 2).Insert(anyButLetterIdx, "[^A-Za-z\\n]");
-            }
-
-            var nextIdx = anyButLetterIdx + 1;
-            if(nextIdx >= regex.Length) break;
-
-            anyButLetterIdx = regex.IndexOf("\\L", nextIdx, StringComparison.InvariantCulture);
-        }
+        // Probably make this slightly saner
+        // Just add chain calls as needed and pray performance doesnt explode
+        regex = regex
+            .Replace("\\l", "[A-Za-z]")
+            .Replace("\\L", "[^A-Za-z\\n]");
     }
 }

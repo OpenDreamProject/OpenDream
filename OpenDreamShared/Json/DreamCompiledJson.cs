b798f71496cb20c109201c57758374c68e1a5027
ZeWaka <zewakagamer@gmail.com>
2023-08-12 12:29:03 -0700

    File-scoped DreamCompiledJson namespace

--- OpenDreamShared/Json/DreamCompiledJson.cs
+++ OpenDreamShared/Json/DreamCompiledJson.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections.Generic;
 
-namespace OpenDreamShared.Json {
-    public sealed class DreamCompiledJson {
-        public List<string>? Strings { get; set; }
-        public string[]? Resources { get; set; }
-        public int[]? GlobalProcs { get; set; }
-        public GlobalListJson? Globals { get; set; }
-        public ProcDefinitionJson? GlobalInitProc { get; set; }
-        public List<DreamMapJson>? Maps { get; set; }
-        public string? Interface { get; set; }
-        public DreamTypeJson[]? Types { get; set; }
-        public ProcDefinitionJson[]? Procs { get; set; }
-    }
+namespace OpenDreamShared.Json;
+
+public sealed class DreamCompiledJson {
+    public List<string>? Strings { get; set; }
+    public string[]? Resources { get; set; }
+    public int[]? GlobalProcs { get; set; }
+    public GlobalListJson? Globals { get; set; }
+    public ProcDefinitionJson? GlobalInitProc { get; set; }
+    public List<DreamMapJson>? Maps { get; set; }
+    public string? Interface { get; set; }
+    public DreamTypeJson[]? Types { get; set; }
+    public ProcDefinitionJson[]? Procs { get; set; }
 }

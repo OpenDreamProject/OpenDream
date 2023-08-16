bae1caffa4961347a22b3ae23cd9236fbc70412c
ZeWaka <zewakagamer@gmail.com>
2023-08-16 07:25:17 -0700

    Adds compiler version metadata to the compiled json (#1416)
    * File-scoped DreamCompiledJson namespace
    
    * Basic opcode hash impl
    
    * Now takes names into account
    
    * Upgrade to error
    
    * Amend message

--- OpenDreamShared/Dream/Procs/DreamProcOpcode.cs
+++ OpenDreamShared/Dream/Procs/DreamProcOpcode.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace OpenDreamShared.Dream.Procs {
@@ -396,6 +397,24 @@ namespace OpenDreamShared.Dream.Procs {
                 bytePool.Add(usedInt);
             }
             return false;
+        }
+
+        /// <summary>
+        /// Calculates a hash of all the <c>DreamProcOpcode</c>s for warning on incompatibilities.
+        /// </summary>
+        /// <returns>A MD5 hash string</returns>
+        public static string GetOpcodesHash() {
+            Array allOpcodes = Enum.GetValues(typeof(DreamProcOpcode));
+            List<byte> opcodesBytes = new List<byte>();
+
+            foreach (var value in allOpcodes) {
+                byte[] nameBytes = Encoding.ASCII.GetBytes(value.ToString()!);
+                opcodesBytes.AddRange(nameBytes);
+                opcodesBytes.Add((byte)value);
+            }
+
+            byte[] hashBytes = MD5.HashData(opcodesBytes.ToArray());
+            return BitConverter.ToString(hashBytes).Replace("-", "");
         }
     }
 }

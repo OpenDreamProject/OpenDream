1a3a2c70d15e26f3cb36416a77814aff4ac34dae
wixoa <wixoag@gmail.com>
2023-10-19 23:38:34 -0400

    Rewrite the package scripts in C# (#1511)
    * Rewrite the package scripts in C#
    
    * Add a TGS package option
    
    * Move to a new `OpenDreamPackageTool` project
    Ran into file locking issues with OpenDreamPackaging as a dependency
    
    * Use RT's `ProcessHelpers.RunCheck()` helper
    
    * Add missing files
    
    * Assume `--platform` argument for TGS
    
    * Remove dot in package dirs
    Brings it closer to the name of the actual projects
    
    * Verify the CWD
    
    * Slight documentation

---
+++ OpenDreamPackageTool/ClientPackaging.cs
@@ -0,0 +1,93 @@
+using System.Diagnostics;
+using Robust.Packaging.Utility;
+
+namespace OpenDreamPackageTool;
+
+/// <summary>
+/// Packages the client's resources & assemblies in a format ready to be downloaded by connecting clients
+/// </summary>
+public static class ClientPackaging {
+    private static readonly string[] ClientIgnoredResources = {
+        "Maps",
+        "emotes.xml",
+        "Groups",
+        "engineCommandPerms.yml",
+        "clientCommandPerms.yml"
+    };
+
+    private static readonly string[] ClientContentAssemblies = {
+        "OpenDreamClient",
+        "OpenDreamShared"
+    };
+
+    public static void Package(Program.ClientOptions options) {
+        Directory.CreateDirectory(options.OutputDir);
+
+        if (!options.SkipBuild)
+            WipeBin();
+
+        Build(options);
+    }
+
+    private static void WipeBin() {
+        Console.WriteLine("Clearing old build artifacts (if any)...");
+
+        Directory.Delete("bin", true);
+    }
+
+    private static void Build(Program.ClientOptions options) {
+        Console.WriteLine("Building project...");
+
+        if (!options.SkipBuild) {
+            ProcessHelpers.RunCheck(new ProcessStartInfo {
+                FileName = "dotnet",
+                ArgumentList = {
+                    "build",
+                    "OpenDreamClient/OpenDreamClient.csproj",
+                    "-c", "Release",
+                    "--nologo",
+                    "/v:m",
+                    "/t:Rebuild",
+                    "/p:FullRelease=True",
+                    "/m"
+                }
+            }).Wait();
+        }
+
+        DirectoryInfo releaseDir = new DirectoryInfo(Path.Combine(options.OutputDir, "OpenDreamClient"));
+
+        Console.WriteLine("Packaging client...");
+        releaseDir.Create();
+        CopyResources(releaseDir.FullName);
+        CopyContentAssemblies(Path.Combine(releaseDir.FullName, "Assemblies"));
+    }
+
+    private static void CopyResources(string dest) {
+        var ignoreSet = Program.SharedIgnoredResources.Union(ClientIgnoredResources).ToArray();
+
+        Program.CopyDirectory("Resources", dest, ignoreSet);
+    }
+
+    private static void CopyContentAssemblies(string dest) {
+        List<string> files = new();
+        string sourceDir = Path.Combine("bin", "Content.Client");
+        string[] baseAssemblies = ClientContentAssemblies;
+
+        // Include content assemblies.
+        foreach (var assembly in baseAssemblies) {
+            files.Add(assembly + ".dll");
+
+            // If PDB available, include it as well.
+            var pdbPath = assembly + ".pdb";
+            if (File.Exists(Path.Combine(sourceDir, pdbPath)))
+                files.Add(pdbPath);
+        }
+
+        // Create assemblies dir if necessary.
+        Directory.CreateDirectory(dest);
+
+        foreach (var file in files) {
+            File.Copy(Path.Combine(sourceDir, file), Path.Combine(dest, file));
+        }
+    }
+}

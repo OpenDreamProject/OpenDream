3ae7a271a52c2c1399d1037741065f4e30a0f913
ike709 <ike709@users.noreply.github.com>
2023-12-02 16:46:34 -0700

    Update DMCompiler/Bytecode/DreamProcOpcode.cs
    Co-authored-by: wixoa <wixoag@gmail.com>

--- DMCompiler/Bytecode/DreamProcOpcode.cs
+++ DMCompiler/Bytecode/DreamProcOpcode.cs
@@ -429,14 +429,11 @@ public struct OpcodeMetadata {
 /// <summary>
 /// Automatically builds a cache of the <see cref="OpcodeMetadata"/> attribute for each opcode
 /// </summary>
-public static class OpcodeMetadataCache
-{
+public static class OpcodeMetadataCache {
     private static readonly OpcodeMetadata[] MetadataCache = new OpcodeMetadata[256];
 
-    static OpcodeMetadataCache()
-    {
-        foreach (DreamProcOpcode opcode in Enum.GetValues(typeof(DreamProcOpcode)))
-        {
+    static OpcodeMetadataCache() {
+        foreach (DreamProcOpcode opcode in Enum.GetValues(typeof(DreamProcOpcode))) {
             var field = typeof(DreamProcOpcode).GetField(opcode.ToString());
             var attribute = Attribute.GetCustomAttribute(field!, typeof(OpcodeMetadataAttribute));
             var metadataAttribute = Unsafe.As<OpcodeMetadataAttribute>(attribute);
@@ -444,8 +441,7 @@ public static class OpcodeMetadataCache
         }
     }
 
-    public static OpcodeMetadata GetMetadata(DreamProcOpcode opcode)
-    {
+    public static OpcodeMetadata GetMetadata(DreamProcOpcode opcode) {
         return MetadataCache[(byte)opcode];
     }
 }

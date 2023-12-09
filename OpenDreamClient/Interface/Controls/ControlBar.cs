91bc01199f69a6af224158ce1e364956d4bc4f5e
wixoaGit <wixoag@gmail.com>
2023-12-09 15:42:18 -0500

    Some code quality

--- OpenDreamClient/Interface/Controls/ControlBar.cs
+++ OpenDreamClient/Interface/Controls/ControlBar.cs
@@ -1,4 +1,3 @@
-using OpenDreamClient.Input;
 using OpenDreamClient.Interface.Descriptors;
 using Robust.Client.Graphics;
 using Robust.Client.UserInterface;
@@ -7,12 +6,11 @@ using Robust.Client.UserInterface.Controls;
 namespace OpenDreamClient.Interface.Controls;
 
 internal sealed class ControlBar : InterfaceControl {
-    private ProgressBar? _bar = null;
-    private Slider? _slider = null;
-    private BoxContainer? _container;
+    private ProgressBar? _bar;
+    private Slider? _slider;
+    private BoxContainer _container = default!; // Created by base constructor
 
-    public ControlBar(ControlDescriptor controlDescriptor, ControlWindow window) :
-        base(controlDescriptor, window) {
+    public ControlBar(ControlDescriptor controlDescriptor, ControlWindow window) : base(controlDescriptor, window) {
     }
 
     protected override void UpdateElementDescriptor() {
@@ -21,8 +19,9 @@ internal sealed class ControlBar : InterfaceControl {
     }
 
     private void OnValueChanged(Robust.Client.UserInterface.Controls.Range range) {
-        if(_slider is not null && _slider.Grabbed) //don't run while you're still sliding, only after
+        if (_slider is not null && _slider.Grabbed) //don't run while you're still sliding, only after
             return;
+
         ControlDescriptorBar controlDescriptor = (ControlDescriptorBar)ElementDescriptor;
         if (controlDescriptor.OnChange != null) {
             _interfaceManager.RunCommand(controlDescriptor.OnChange);
@@ -31,8 +30,7 @@ internal sealed class ControlBar : InterfaceControl {
 
 
     protected override Control CreateUIElement() {
-        ControlDescriptorBar controlDescriptor = (ControlDescriptorBar)ElementDescriptor;
-        _container = new BoxContainer() {Orientation = BoxContainer.LayoutOrientation.Vertical};
+        _container = new BoxContainer { Orientation = BoxContainer.LayoutOrientation.Vertical };
         _container.OnResized += OnContainerResized;
         return _container;
     }
@@ -43,58 +41,75 @@ internal sealed class ControlBar : InterfaceControl {
         //TODO angles
 
         //width
-        float bar_width = controlDescriptor.Width != null ? (float)controlDescriptor.Width : 10;
-        if(bar_width == 0 && this._container is not null)
-            bar_width = _container.Size.Y;
+        float barWidth = controlDescriptor.Width ?? 10f;
+        if (barWidth == 0)
+            barWidth = _container.Size.Y;
 
         //is-slider
-        if(controlDescriptor.IsSlider){
-            if(_slider is null){
-                _slider = new Slider() {MaxValue = 100, MinValue = 0, Margin=new Thickness(4), HorizontalExpand=true, MinHeight=bar_width};
-                if(_bar is not null){
-                    _container!.RemoveChild(_bar);
+        if (controlDescriptor.IsSlider) {
+            if (_slider is null) {
+                _slider = new Slider {
+                    MaxValue = 100,
+                    MinValue = 0,
+                    Margin = new Thickness(4),
+                    HorizontalExpand = true,
+                    MinHeight = barWidth
+                };
+
+                if (_bar is not null) {
+                    _container.RemoveChild(_bar);
                     _bar = null;
                 }
-                _container!.AddChild(_slider);
+
+                _container.AddChild(_slider);
             } else {
-                _slider.SetHeight=bar_width;
+                _slider.SetHeight = barWidth;
             }
 
             //value
-            _slider.Value = controlDescriptor.Value == null ? 0.0f : (float)controlDescriptor.Value;
+            _slider.Value = controlDescriptor.Value ?? 0.0f;
             //on-change
             _slider.OnValueChanged += OnValueChanged;
             //bar-color
             _slider.TryGetStyleProperty<StyleBox>(Slider.StylePropertyGrabber, out var box);
-            if(box is not null){
-                StyleBoxFlat boxflat = (StyleBoxFlat) box;
-                boxflat.BackgroundColor = controlDescriptor.BarColor is null ? Color.Transparent : controlDescriptor.BarColor.Value;
-                _slider.GrabberStyleBoxOverride = boxflat;
+            if (box is not null) {
+                StyleBoxFlat boxFlat = (StyleBoxFlat)box;
+                boxFlat.BackgroundColor = controlDescriptor.BarColor ?? Color.Transparent;
+                _slider.GrabberStyleBoxOverride = boxFlat;
             }
         } else {
-            if(_bar is null){
-                _bar = new ProgressBar() {MaxValue = 100, MinValue = 0, Margin=new Thickness(4), HorizontalExpand=true, MinHeight=bar_width};
-                if(_slider is not null){
-                    _container!.RemoveChild(_slider);
+            if (_bar is null) {
+                _bar = new ProgressBar {
+                    MaxValue = 100,
+                    MinValue = 0,
+                    Margin = new Thickness(4),
+                    HorizontalExpand = true,
+                    MinHeight = barWidth
+                };
+
+                if (_slider is not null) {
+                    _container.RemoveChild(_slider);
                     _slider = null;
                 }
-                _container!.AddChild(_bar);
+
+                _container.AddChild(_bar);
             } else {
-                _bar.SetHeight=bar_width;
+                _bar.SetHeight = barWidth;
             }
+
             //on-change
             if (controlDescriptor.OnChange != null && _bar.Value != controlDescriptor.Value) {
                 _interfaceManager.RunCommand(controlDescriptor.OnChange);
             }
+
             //bar-color
-            _bar.TryGetStyleProperty<StyleBoxFlat>(ProgressBar.StylePropertyForeground , out var box);
-            if(box is not null){
-                StyleBoxFlat boxflat = (StyleBoxFlat) box;
-                boxflat.BackgroundColor = controlDescriptor.BarColor is null ? Color.Transparent : controlDescriptor.BarColor.Value;
-                _bar.ForegroundStyleBoxOverride = boxflat;
+            if (_bar.TryGetStyleProperty<StyleBoxFlat>(ProgressBar.StylePropertyForeground, out var box)) {
+                box.BackgroundColor = controlDescriptor.BarColor ?? Color.Transparent;
+                _bar.ForegroundStyleBoxOverride = box;
             }
+
             //value
-            _bar.Value = controlDescriptor.Value == null ? 0.0f : (float)controlDescriptor.Value;
+            _bar.Value = controlDescriptor.Value ?? 0.0f;
         }
     }
 }

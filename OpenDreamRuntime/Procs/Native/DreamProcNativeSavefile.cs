a3014f14c1d19c10dddf7ac9554944dda9bc4d74
amylizzle <amylizzlep@gmail.com>
2023-12-14 16:02:16 +0000

    stub import

--- OpenDreamRuntime/Procs/Native/DreamProcNativeSavefile.cs
+++ OpenDreamRuntime/Procs/Native/DreamProcNativeSavefile.cs
@@ -3,7 +3,6 @@ using OpenDreamRuntime.Objects.Types;
 using OpenDreamRuntime.Resources;
 using DreamValueTypeFlag = OpenDreamRuntime.DreamValue.DreamValueTypeFlag;
 using System.IO;
-using YamlDotNet.Core.Tokens;
 
 namespace OpenDreamRuntime.Procs.Native;
 
@@ -24,6 +23,24 @@ internal static class DreamProcNativeSavefile {
         string result = ExportTextInternal(savefile);
         //TODO if file is not null, write to file
         return new DreamValue(result);
+    }
+
+    [DreamProc("ImportText")]
+    [DreamProcParameter("path", Type = DreamValueTypeFlag.String)]
+    [DreamProcParameter("source", Type = DreamValueTypeFlag.String | DreamValueTypeFlag.DreamResource)]
+    public static DreamValue NativeProc_ImportText(NativeProc.Bundle bundle, DreamObject? src, DreamObject? usr) {
+
+        var savefile = (DreamObjectSavefile)src!;
+        DreamValue path = bundle.GetArgument(0, "path");
+        DreamValue source = bundle.GetArgument(1, "source");
+
+        if(!path.IsNull && path.TryGetValueAsString(out var pathStr)) { //invalid path values are just ignored in BYOND
+            savefile.ChangeDirectory(pathStr);
+        }
+        //if source is a file, read text from file and parse
+        //else if source is a string, parse string
+        //savefile.OperatorOutput(new DreamValue(source));
+        return DreamValue.Null;
     }
 
 

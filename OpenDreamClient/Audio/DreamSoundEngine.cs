cd8892fc50ed5c20ff113ad8cd80874163a25e3e
wixoaGit <wixoag@gmail.com>
2023-12-22 16:44:50 -0500

    Some file-scoped namespaces

--- OpenDreamClient/Audio/DreamSoundEngine.cs
+++ OpenDreamClient/Audio/DreamSoundEngine.cs
@@ -5,99 +5,99 @@ using Robust.Client.Audio;
 using Robust.Shared.Audio;
 using Robust.Shared.Network;
 
-namespace OpenDreamClient.Audio {
-    public sealed class DreamSoundEngine : IDreamSoundEngine {
-        private const int SoundChannelLimit = 1024;
+namespace OpenDreamClient.Audio;
 
-        [Dependency] private readonly IDreamResourceManager _resourceManager = default!;
-        [Dependency] private readonly ILogManager _logManager = default!;
-        [Dependency] private readonly INetManager _netManager = default!;
-        [Dependency] private readonly IEntitySystemManager _entitySystemManager = default!;
-        [Dependency] private readonly IAudioManager _audioManager = default!;
-        private AudioSystem? _audioSystem;
+public sealed class DreamSoundEngine : IDreamSoundEngine {
+    private const int SoundChannelLimit = 1024;
 
-        private ISawmill _sawmill = default!;
+    [Dependency] private readonly IDreamResourceManager _resourceManager = default!;
+    [Dependency] private readonly ILogManager _logManager = default!;
+    [Dependency] private readonly INetManager _netManager = default!;
+    [Dependency] private readonly IEntitySystemManager _entitySystemManager = default!;
+    [Dependency] private readonly IAudioManager _audioManager = default!;
+    private AudioSystem? _audioSystem;
 
-        private readonly DreamSoundChannel?[] _channels = new DreamSoundChannel[SoundChannelLimit];
+    private ISawmill _sawmill = default!;
 
-        public void Initialize() {
-            _sawmill = _logManager.GetSawmill("opendream.audio");
+    private readonly DreamSoundChannel?[] _channels = new DreamSoundChannel[SoundChannelLimit];
 
-            _netManager.RegisterNetMessage<MsgSound>(RxSound);
+    public void Initialize() {
+        _sawmill = _logManager.GetSawmill("opendream.audio");
 
-            _netManager.Disconnect += DisconnectedFromServer;
+        _netManager.RegisterNetMessage<MsgSound>(RxSound);
+
+        _netManager.Disconnect += DisconnectedFromServer;
+    }
+
+    public void StopFinishedChannels() {
+        for (int i = 0; i < SoundChannelLimit; i++) {
+            if (_channels[i]?.Source.Component.Playing is false or null)
+                StopChannel(i + 1);
         }
+    }
 
-        public void StopFinishedChannels() {
-            for (int i = 0; i < SoundChannelLimit; i++) {
-                if (_channels[i]?.Source.Component.Playing is false or null)
-                    StopChannel(i + 1);
+    public void PlaySound(int channel, MsgSound.FormatType format, ResourceSound sound, float volume) {
+        if (_audioSystem == null)
+            _entitySystemManager.Resolve(ref _audioSystem);
+
+        if (channel == 0) {
+            //First available channel
+            for (int i = 0; i < _channels.Length; i++) {
+                if (_channels[i] == null) {
+                    channel = i + 1;
+                    break;
+                }
             }
-        }
-
-        public void PlaySound(int channel, MsgSound.FormatType format, ResourceSound sound, float volume) {
-            if (_audioSystem == null)
-                _entitySystemManager.Resolve(ref _audioSystem);
 
             if (channel == 0) {
-                //First available channel
-                for (int i = 0; i < _channels.Length; i++) {
-                    if (_channels[i] == null) {
-                        channel = i + 1;
-                        break;
-                    }
-                }
-
-                if (channel == 0) {
-                    _sawmill.Error("Failed to find a free audio channel to play a sound on");
-                    return;
-                }
-            }
-
-            StopChannel(channel);
-
-            var stream = sound.GetStream(format, _audioManager);
-            if (stream == null) {
-                _sawmill.Error($"Failed to load audio ${sound}");
+                _sawmill.Error("Failed to find a free audio channel to play a sound on");
                 return;
             }
-
-            var db = 20 * MathF.Log10(volume); // convert from DM volume (0-100) to OpenAL volume (db)
-            var source = _audioSystem.PlayGlobal(stream, AudioParams.Default.WithVolume(db)); // TODO: Positional audio.
-            if (source == null) {
-                _sawmill.Error($"Failed to play audio ${sound}");
-                return;
-            }
-
-            _channels[channel - 1] = new DreamSoundChannel(_audioSystem, source.Value);
         }
 
+        StopChannel(channel);
 
-        public void StopChannel(int channel) {
-            ref DreamSoundChannel? ch = ref _channels[channel - 1];
-
-            ch?.Stop();
-            // This will null the corresponding index in the array.
-            ch = null;
+        var stream = sound.GetStream(format, _audioManager);
+        if (stream == null) {
+            _sawmill.Error($"Failed to load audio ${sound}");
+            return;
         }
 
-        public void StopAllChannels() {
-            for (int i = 0; i < SoundChannelLimit; i++) {
-                StopChannel(i + 1);
-            }
+        var db = 20 * MathF.Log10(volume); // convert from DM volume (0-100) to OpenAL volume (db)
+        var source = _audioSystem.PlayGlobal(stream, AudioParams.Default.WithVolume(db)); // TODO: Positional audio.
+        if (source == null) {
+            _sawmill.Error($"Failed to play audio ${sound}");
+            return;
         }
 
-        private void RxSound(MsgSound msg) {
-            if (msg.ResourceId.HasValue) {
-                _resourceManager.LoadResourceAsync<ResourceSound>(msg.ResourceId.Value,
-                    sound => PlaySound(msg.Channel, msg.Format!.Value, sound, msg.Volume / 100.0f));
-            } else {
-                StopChannel(msg.Channel);
-            }
-        }
+        _channels[channel - 1] = new DreamSoundChannel(_audioSystem, source.Value);
+    }
 
-        private void DisconnectedFromServer(object? sender, NetDisconnectedArgs e) {
-            StopAllChannels();
+
+    public void StopChannel(int channel) {
+        ref DreamSoundChannel? ch = ref _channels[channel - 1];
+
+        ch?.Stop();
+        // This will null the corresponding index in the array.
+        ch = null;
+    }
+
+    public void StopAllChannels() {
+        for (int i = 0; i < SoundChannelLimit; i++) {
+            StopChannel(i + 1);
         }
+    }
+
+    private void RxSound(MsgSound msg) {
+        if (msg.ResourceId.HasValue) {
+            _resourceManager.LoadResourceAsync<ResourceSound>(msg.ResourceId.Value,
+                sound => PlaySound(msg.Channel, msg.Format!.Value, sound, msg.Volume / 100.0f));
+        } else {
+            StopChannel(msg.Channel);
+        }
+    }
+
+    private void DisconnectedFromServer(object? sender, NetDisconnectedArgs e) {
+        StopAllChannels();
     }
 }
--- OpenDreamClient/Resources/ResourceTypes/ResourceSound.cs
+++ OpenDreamClient/Resources/ResourceTypes/ResourceSound.cs
@@ -3,27 +3,27 @@ using JetBrains.Annotations;
 using OpenDreamShared.Network.Messages;
 using Robust.Client.Audio;
 
-namespace OpenDreamClient.Resources.ResourceTypes {
-    [UsedImplicitly]
-    public sealed class ResourceSound(int id, byte[] data) : DreamResource(id, data) {
-        private AudioStream? _stream;
+namespace OpenDreamClient.Resources.ResourceTypes;
 
-        public AudioStream? GetStream(MsgSound.FormatType format, IAudioManager audioManager) {
-            if (_stream == null) {
-                switch (format) {
-                    case MsgSound.FormatType.Ogg:
-                        _stream = audioManager.LoadAudioOggVorbis(new MemoryStream(Data));
-                        break;
-                    case MsgSound.FormatType.Wav:
-                        _stream = audioManager.LoadAudioWav(new MemoryStream(Data));
-                        break;
-                    default:
-                        Logger.GetSawmill("opendream.audio").Fatal("Only *.ogg and *.wav audio files are supported.");
-                        break;
-                }
+[UsedImplicitly]
+public sealed class ResourceSound(int id, byte[] data) : DreamResource(id, data) {
+    private AudioStream? _stream;
+
+    public AudioStream? GetStream(MsgSound.FormatType format, IAudioManager audioManager) {
+        if (_stream == null) {
+            switch (format) {
+                case MsgSound.FormatType.Ogg:
+                    _stream = audioManager.LoadAudioOggVorbis(new MemoryStream(Data));
+                    break;
+                case MsgSound.FormatType.Wav:
+                    _stream = audioManager.LoadAudioWav(new MemoryStream(Data));
+                    break;
+                default:
+                    Logger.GetSawmill("opendream.audio").Fatal("Only *.ogg and *.wav audio files are supported.");
+                    break;
             }
-
-            return _stream;
         }
+
+        return _stream;
     }
 }

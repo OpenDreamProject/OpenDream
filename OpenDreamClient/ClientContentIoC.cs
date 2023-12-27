bdd8cd52207958fb4b295982818e7c91bfc559b1
LetterN <24603524+LetterN@users.noreply.github.com>
2023-12-27 13:21:24 +0800

    cleanup EntryPoint.cs + move this to maek style load

--- OpenDreamClient/ClientContentIoC.cs
+++ OpenDreamClient/ClientContentIoC.cs
@@ -4,14 +4,14 @@ using OpenDreamClient.Interface;
 using OpenDreamClient.Resources;
 using OpenDreamClient.States;
 
-namespace OpenDreamClient {
-    public static class ClientContentIoC {
-        public static void Register() {
-            IoCManager.Register<IDreamInterfaceManager, DreamInterfaceManager>();
-            IoCManager.Register<IClickMapManager, ClickMapManager>();
-            IoCManager.Register<IDreamResourceManager, DreamResourceManager>();
-            IoCManager.Register<DreamUserInterfaceStateManager>();
-            IoCManager.Register<IDreamSoundEngine, DreamSoundEngine>();
-        }
+namespace OpenDreamClient;
+
+internal static class ClientContentIoC {
+    public static void Register() {
+        IoCManager.Register<IDreamInterfaceManager, DreamInterfaceManager>();
+        IoCManager.Register<IClickMapManager, ClickMapManager>();
+        IoCManager.Register<IDreamResourceManager, DreamResourceManager>();
+        IoCManager.Register<DreamUserInterfaceStateManager>();
+        IoCManager.Register<IDreamSoundEngine, DreamSoundEngine>();
     }
 }

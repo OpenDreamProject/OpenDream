685191e21dbe65a112d9a33ecf908f5a93fdb914
wixoaGit <wixoag@gmail.com>
2024-02-20 20:21:11 -0500

    Add support for the TAB control

--- OpenDreamClient/Interface/Controls/ControlBrowser.cs
+++ OpenDreamClient/Interface/Controls/ControlBrowser.cs
@@ -53,10 +53,12 @@ internal sealed class ControlBrowser : InterfaceControl {
                 OnHideEvent();
             }
         };
+
         if(ControlDescriptor.IsVisible)
             OnShowEvent();
         else
             OnHideEvent();
+
         return _webView;
     }
 
@@ -157,14 +159,14 @@ internal sealed class ControlBrowser : InterfaceControl {
         _interfaceManager.WinSet(element, modifiedQuery);
     }
 
-    public void OnShowEvent() {
+    private void OnShowEvent() {
         ControlDescriptorBrowser controlDescriptor = (ControlDescriptorBrowser)ControlDescriptor;
         if (!string.IsNullOrWhiteSpace(controlDescriptor.OnShowCommand)) {
             _interfaceManager.RunCommand(controlDescriptor.OnShowCommand);
         }
     }
 
-    public void OnHideEvent() {
+    private void OnHideEvent() {
         ControlDescriptorBrowser controlDescriptor = (ControlDescriptorBrowser)ControlDescriptor;
         if (!string.IsNullOrWhiteSpace(controlDescriptor.OnHideCommand)) {
             _interfaceManager.RunCommand(controlDescriptor.OnHideCommand);

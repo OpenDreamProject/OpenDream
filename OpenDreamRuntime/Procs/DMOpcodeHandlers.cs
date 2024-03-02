39f99aca0b6a136f6c007c086a2cb48bb1fb31c8
Amy <3855802+amylizzle@users.noreply.github.com>
2024-03-02 00:10:01 +0000

    Fully implement `/savefile` (except `ImportText()` because fuck it) (#1554)
    Co-authored-by: LetterN <24603524+LetterN@users.noreply.github.com>
    Co-authored-by: wixoa <wixoag@gmail.com>
    Co-authored-by: amylizzle <amylizzle@users.noreply.github.com>

--- OpenDreamRuntime/Procs/DMOpcodeHandlers.cs
+++ OpenDreamRuntime/Procs/DMOpcodeHandlers.cs
@@ -2104,6 +2104,11 @@ namespace OpenDreamRuntime.Procs {
                     state.GetReferenceValue(leftRef);
                     state.GetReferenceValue(rightRef);
                 }
+            } else if (state.GetReferenceValue(leftRef).TryGetValueAsDreamObject<DreamObjectSavefile>(out var savefile)) {
+                // Savefiles get some special treatment.
+                // "savefile >> B" is the same as "B = savefile[current_dir]"
+                state.AssignReference(rightRef, savefile.OperatorInput());
+                return ProcStatus.Continue;
             }
 
             throw new NotImplementedException($"Input operation is unimplemented for {leftRef} and {rightRef}");

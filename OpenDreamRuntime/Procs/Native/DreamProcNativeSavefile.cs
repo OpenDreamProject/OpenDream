925c643e5bc76dee065708e6d3619e1b064bd90c
John Doe <gamingskeleton3@gmail.com>
2024-03-04 20:39:10 -0800

    implements ImportText

--- OpenDreamRuntime/Procs/Native/DreamProcNativeSavefile.cs
+++ OpenDreamRuntime/Procs/Native/DreamProcNativeSavefile.cs
@@ -131,6 +131,113 @@ internal static class DreamProcNativeSavefile {
 
     }
 
+    [DreamProc("ImportText")]
+    [DreamProcParameter("path", Type = DreamValueTypeFlag.String)]
+    [DreamProcParameter("source", Type = DreamValueTypeFlag.String | DreamValueTypeFlag.DreamResource)]
+    public static DreamValue NativeProc_ImportText(NativeProc.Bundle bundle, DreamObject? src, DreamObject? usr) {
+        var savefile = (DreamObjectSavefile)src!;
+        DreamValue path = bundle.GetArgument(0, "path");
+        DreamValue source = bundle.GetArgument(1, "source");
+
+        string oldPath = savefile.CurrentPath;
+        if(!path.IsNull && path.TryGetValueAsString(out var pathStr)) { //invalid path values are just ignored in BYOND
+            savefile.CurrentPath = pathStr;
+        }
+
+        int result = ImportTextInternal(savefile, source, bundle);
+
+        savefile.CurrentPath = oldPath; //restore current directory after a query
+        return new DreamValue(result);
+    }
+
+    private static int ImportTextInternal(DreamObjectSavefile savefile, DreamValue source, NativeProc.Bundle bundle) {
+        if(source.IsNull) {
+            return 0;
+        }
+
+        if(source.TryGetValueAsString(out var sourceStr)) {
+            return ImportTextParseLine(sourceStr, savefile, bundle);
+        } else if(source.TryGetValueAsDreamResource(out var fileResource)) {
+            if(fileResource.ResourceData == null) {
+                return 0;
+            }
+
+            string fileString = fileResource.ReadAsString()!;
+            string[] fileStrings = fileString.Split("\n");
+            bool returnedFalse = false;
+            foreach(string fileLine in fileStrings) {
+                int returnValue = ImportTextParseLine(fileLine, savefile, bundle);
+                if(returnValue == 0) {
+                    returnedFalse = true;
+                }
+            }
+            if(returnedFalse) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+
+    private static int ImportTextParseLine(string lineToParse, DreamObjectSavefile savefile, NativeProc.Bundle bundle) {
+        // ; is used to split individual savefile entries in ImportText()
+        string[] savefileImports = lineToParse.Split(";");
+        foreach(string savefileEntry in savefileImports) {
+            // We cannot split by " = " or similar because we cannot guarantee there will be only one = or that there is consistent spacing
+            string[] spaceSplit = savefileEntry.Split(" ");
+            bool foundEquals = false;
+            string foundValue = "";
+            string foundIndex = "";
+            for(int i = 0; i < spaceSplit.Length; i++) {
+                if(i == 0) {
+                    foundIndex = spaceSplit[i];
+                    continue;
+                }
+                string individualWord = spaceSplit[i];
+                if(individualWord == "=") {
+                    // "entry = ="
+                    if(foundEquals) {
+                        bundle.DreamManager.WriteWorldLog($"failed to parse savefile text at line 0 (reading 'end of file'): expecting ;");
+                    }
+                    foundEquals = true;
+                    continue;
+                }
+                // "entry = a b"
+                if(spaceSplit[i - 1] != "=") {
+                    bundle.DreamManager.WriteWorldLog($"failed to parse savefile text at line 0 (reading 'end of file'): expecting ;");
+                } else {
+                    foundValue = individualWord;
+                }
+            }
+
+            // "entry ="
+            if(foundEquals && (foundValue == "")) {
+                bundle.DreamManager.WriteWorldLog($"failed to parse savefile text at line 0 (reading 'end of file'): expecting ;");
+            }
+
+            if(!foundEquals && (foundValue == "")) {
+                //todo: sort out the difference between "key = null" and "key"
+                savefile.SetSavefileValue(foundIndex, DreamValue.Null);
+            }
+
+            if((foundValue != "") && (foundIndex != "")) {
+                bool canConvert = float.TryParse(foundValue, out float possibleNumber);
+                // DM implicitly converts values to numbers if possible
+                if (canConvert) {
+                    DreamValue value = new(possibleNumber);
+                    savefile.SetSavefileValue(foundIndex, value);
+                } else {
+                    DreamValue value = new(foundValue);
+                    savefile.SetSavefileValue(foundIndex, value);
+                }
+            }
+
+        }
+        return 1;
+    }
+
     [DreamProc("Flush")]
     public static DreamValue NativeProc_Flush(NativeProc.Bundle bundle, DreamObject? src, DreamObject? usr) {
         var savefile = (DreamObjectSavefile)src!;

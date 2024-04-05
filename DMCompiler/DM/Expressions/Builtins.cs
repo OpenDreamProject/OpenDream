2d7ce6bc2445bb86be2a6dff968c092784cd4632
Penelope Haze <out.of.p.haze@proton.me>
2024-04-05 18:55:12 -0400

    Fix type inference for New

--- DMCompiler/DM/Expressions/Builtins.cs
+++ DMCompiler/DM/Expressions/Builtins.cs
@@ -47,6 +47,7 @@ namespace DMCompiler.DM.Expressions {
     sealed class New : DMExpression {
         private readonly DMExpression _expr;
         private readonly ArgumentList _arguments;
+        public override DMValueType ValType => Path?.GetATOMType() ?? DMValueType.Anything;
 
         public override bool PathIsFuzzy => Path == null;
 

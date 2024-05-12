81615bbabafea131f171580300d09e4fd8c0cdb6
Amylizzle <amylizzlep@gmail.com>
2024-05-12 19:15:22 +0100

    namespace

--- OpenDreamShared/Network/Messages/MsgNotifyResourceUpdate.cs
+++ OpenDreamShared/Network/Messages/MsgNotifyResourceUpdate.cs
@@ -3,18 +3,18 @@ using Lidgren.Network;
 using Robust.Shared.Network;
 using Robust.Shared.Serialization;
 
-namespace OpenDreamShared.Network.Messages {
-    public sealed class MsgNotifyResourceUpdate : NetMessage {
-        public override MsgGroups MsgGroup => MsgGroups.EntityEvent;
+namespace OpenDreamShared.Network.Messages
+public sealed class MsgNotifyResourceUpdate : NetMessage {
+    public override MsgGroups MsgGroup => MsgGroups.EntityEvent;
 
-        public int ResourceId;
+    public int ResourceId;
 
-        public override void ReadFromBuffer(NetIncomingMessage buffer, IRobustSerializer serializer) {
-            ResourceId = buffer.ReadInt32();
-        }
+    public override void ReadFromBuffer(NetIncomingMessage buffer, IRobustSerializer serializer) {
+        ResourceId = buffer.ReadInt32();
+    }
 
-        public override void WriteToBuffer(NetOutgoingMessage buffer, IRobustSerializer serializer) {
-            buffer.Write(ResourceId);
-        }
+    public override void WriteToBuffer(NetOutgoingMessage buffer, IRobustSerializer serializer) {
+        buffer.Write(ResourceId);
     }
 }
+

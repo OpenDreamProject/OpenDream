956f9cf1c3fde9f20f2fbec73cae32a4a5ca8ce8
Redmoogle <redmoogle@rednako.xyz>
2024-05-18 19:02:08 -0400

    remove dodgy behaviour

--- Tools/clean_logs.py
+++ Tools/clean_logs.py
@@ -9,11 +9,13 @@ if __name__ == "__main__":
     warning_pattern = re.compile(r'##\[warning\].*') # catch warnings might want to catch errors?
 
     for log_file in log_files:
-        with open(log_file, 'w+') as log:
-            for line in log:
+        with open(log_file, 'r') as log_in:
+            lines = log_in.readlines()
+        with open(log_file, 'w') as log_out:
+            for line in lines:
                 if warning_pattern.search(line):
                     if line not in seen:
                         seen.add(line)
-                        log.write(line)
-                else:
-                    log.write(line)
+                        log_out.write(line)
+                    continue
+                log_out.write(line)

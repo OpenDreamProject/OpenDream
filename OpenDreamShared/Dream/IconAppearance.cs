07dba39429d7378d25c5a6570c8257af618e6dbd
wixoaGit <wixoag@gmail.com>
2024-06-11 19:20:31 -0400

    Fix several ReSharper issues in IconAppearance

--- OpenDreamShared/Dream/IconAppearance.cs
+++ OpenDreamShared/Dream/IconAppearance.cs
@@ -21,18 +21,6 @@ public sealed class IconAppearance : IEquatable<IconAppearance> {
     [ViewVariables] public Color Color = Color.White;
     [ViewVariables] public byte Alpha = 255;
     [ViewVariables] public float GlideSize;
-    /// <summary>
-    /// An appearance can gain a color matrix filter by two possible forces: <br/>
-    /// 1. the /atom.color var is modified. <br/>
-    /// 2. the /atom.filters var gets a new filter of type "color". <br/>
-    /// DM crashes in some circumstances of this but we, as an extension :^), should try not to. <br/>
-    /// So, this exists as a way for the appearance to remember whether it's coloured by .color, specifically.
-    /// </summary>
-    /// <remarks>
-    /// The reason we don't just take the slow path and always use this filter is not just for optimization,<br/>
-    /// it's also for parity! See <see cref="TryRepresentMatrixAsRGBAColor(in ColorMatrix, out Color?)"/> for more.
-    /// </remarks>
-    [ViewVariables] public ColorMatrix ColorMatrix = ColorMatrix.Identity;
     [ViewVariables] public float Layer = -1f;
     [ViewVariables] public int Plane = -32767;
     [ViewVariables] public BlendMode BlendMode = BlendMode.Default;
@@ -48,6 +36,20 @@ public sealed class IconAppearance : IEquatable<IconAppearance> {
     [ViewVariables] public List<NetEntity> VisContents;
     [ViewVariables] public List<DreamFilter> Filters;
     [ViewVariables] public List<int> Verbs;
+
+    /// <summary>
+    /// An appearance can gain a color matrix filter by two possible forces: <br/>
+    /// 1. the /atom.color var is modified. <br/>
+    /// 2. the /atom.filters var gets a new filter of type "color". <br/>
+    /// DM crashes in some circumstances of this but we, as an extension :^), should try not to. <br/>
+    /// So, this exists as a way for the appearance to remember whether it's coloured by .color, specifically.
+    /// </summary>
+    /// <remarks>
+    /// The reason we don't just take the slow path and always use this filter is not just for optimization,<br/>
+    /// it's also for parity! See <see cref="TryRepresentMatrixAsRgbaColor"/> for more.
+    /// </remarks>
+    [ViewVariables] public ColorMatrix ColorMatrix = ColorMatrix.Identity;
+
     /// <summary> The Transform property of this appearance, in [a,d,b,e,c,f] order</summary>
     [ViewVariables] public float[] Transform = [
         1, 0,   // a d
@@ -82,11 +84,11 @@ public sealed class IconAppearance : IEquatable<IconAppearance> {
         Invisibility = appearance.Invisibility;
         Opacity = appearance.Opacity;
         MouseOpacity = appearance.MouseOpacity;
-        Overlays = new(appearance.Overlays);
-        Underlays = new(appearance.Underlays);
-        VisContents = new(appearance.VisContents);
-        Filters = new(appearance.Filters);
-        Verbs = new(appearance.Verbs);
+        Overlays = [..appearance.Overlays];
+        Underlays = [..appearance.Underlays];
+        VisContents = [..appearance.VisContents];
+        Filters = [..appearance.Filters];
+        Verbs = [..appearance.Verbs];
         Override = appearance.Override;
 
         for (int i = 0; i < 6; i++) {
@@ -106,7 +108,7 @@ public sealed class IconAppearance : IEquatable<IconAppearance> {
         if (appearance.PixelOffset != PixelOffset) return false;
         if (appearance.Color != Color) return false;
         if (appearance.Alpha != Alpha) return false;
-        if (appearance.GlideSize != GlideSize) return false;
+        if (!appearance.GlideSize.Equals(GlideSize)) return false;
         if (!appearance.ColorMatrix.Equals(ColorMatrix)) return false;
         if (!appearance.Layer.Equals(Layer)) return false;
         if (appearance.Plane != Plane) return false;
@@ -157,8 +159,9 @@ public sealed class IconAppearance : IEquatable<IconAppearance> {
     /// then it is coerced into one internally before being saved onto some appearance. <br/>
     /// This does the linear algebra madness necessary to determine whether this is the case or not.
     /// </summary>
-    private static bool TryRepresentMatrixAsRGBAColor(in ColorMatrix matrix, [NotNullWhen(true)] out Color? maybeColor) {
+    private static bool TryRepresentMatrixAsRgbaColor(in ColorMatrix matrix, [NotNullWhen(true)] out Color? maybeColor) {
         maybeColor = null;
+
         // The R G B A values need to be bounded [0,1] for a color conversion to work;
         // anything higher implies trying to render "superblue" or something.
         float diagonalSum = 0f;
@@ -167,6 +170,7 @@ public sealed class IconAppearance : IEquatable<IconAppearance> {
                 return false;
             diagonalSum += diagonalValue;
         }
+
         // and then all of the other values need to be zero, including the offset vector.
         float sum = 0f;
         foreach (float value in matrix.GetValues()) {
@@ -174,6 +178,7 @@ public sealed class IconAppearance : IEquatable<IconAppearance> {
                 return false;
             sum += value;
         }
+
         if (sum - diagonalSum == 0) // PREEETTY sure I can trust the floating-point math here. Not 100% though
             maybeColor = new Color(matrix.c11, matrix.c22, matrix.c33, matrix.c44);
         return maybeColor is not null;
@@ -245,7 +250,7 @@ public sealed class IconAppearance : IEquatable<IconAppearance> {
     /// Sets the 'color' attribute to a color matrix, which will be used on the icon later on by a shader.
     /// </summary>
     public void SetColor(in ColorMatrix matrix) {
-        if (TryRepresentMatrixAsRGBAColor(matrix, out var matrixColor)) {
+        if (TryRepresentMatrixAsRgbaColor(matrix, out var matrixColor)) {
             Color = matrixColor.Value;
             ColorMatrix = ColorMatrix.Identity;
             return;
@@ -305,6 +310,5 @@ public enum AnimationFlags {
     AnimationParallel = 4,
     AnimationSlice = 8,
     AnimationRelative = 256,
-    AnimationContinue = 512,
-
+    AnimationContinue = 512
 }

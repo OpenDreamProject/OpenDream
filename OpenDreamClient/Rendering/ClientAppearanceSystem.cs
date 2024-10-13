b2d76af73364d2a86ab7405d90bf869105780c64
amylizzlep <amylizzlep@gmail.com>
2024-10-13 17:47:36 +0100

    missed one

--- OpenDreamClient/Rendering/ClientAppearanceSystem.cs
+++ OpenDreamClient/Rendering/ClientAppearanceSystem.cs
@@ -78,8 +78,8 @@ internal sealed class ClientAppearanceSystem : SharedAppearanceSystem {
     }
 
     private void OnAnimation(AnimationEvent e) {
-        if(e.Entity == NetEntity.Invalid && e.turfId is not null) { //it's a turf or area
-            if(_turfIcons.TryGetValue(e.turfId.Value, out var turfIcon))
+        if(e.Entity == NetEntity.Invalid && e.TurfId is not null) { //it's a turf or area
+            if(_turfIcons.TryGetValue(e.TurfId.Value, out var turfIcon))
                 LoadAppearance(e.TargetAppearanceId, targetAppearance => {
                     turfIcon.StartAppearanceAnimation(targetAppearance, e.Duration, e.Easing, e.Loop, e.Flags, e.Delay, e.ChainAnim);
                 });

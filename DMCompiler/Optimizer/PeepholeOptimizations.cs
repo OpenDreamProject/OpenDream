1073cc3943209f0b9f6cf526e19a29491f9ad928
ike709 <ike709@github.com>
2024-10-25 15:16:41 -0500

    fix compilation?

--- DMCompiler/Optimizer/PeepholeOptimizations.cs
+++ DMCompiler/Optimizer/PeepholeOptimizations.cs
@@ -778,7 +778,7 @@ internal sealed class ConstFoldBitshiftLeft : IPeepholeOptimization {
 
         // At runtime, given "A << B" we pop B then A
         // In the peephole optimizer, index is "A", index+1 is "B"
-        var args = new List<IAnnotatedBytecode>(1) {new AnnotatedBytecodeInteger(((int)pushVal1 << (int)pushVal2), firstInstruction.Location)};
+        var args = new List<IAnnotatedBytecode>(1) {new AnnotatedBytecodeFloat(((int)pushVal1 << (int)pushVal2), firstInstruction.Location)};
 
         IPeepholeOptimization.ReplaceInstructions(input, index, 3,
             new AnnotatedBytecodeInstruction(DreamProcOpcode.PushFloat, 1, args));

a27d9ae9af421d83eab90d179a153d8d17ccc082
ike709 <ike709@users.noreply.github.com>
2024-10-28 14:15:48 -0500

    did I say AppendNoPush? I meant AssignNoPush

--- DMCompiler/Optimizer/PeepholeOptimizations.cs
+++ DMCompiler/Optimizer/PeepholeOptimizations.cs
@@ -776,14 +776,14 @@ internal sealed class ConstFoldPower : IPeepholeOptimization {
     }
 }
 
-// AppendNoPush [ref]
+// AssignNoPush [ref]
 // PushReferenceValue [ref]
 // -> Assign [ref]
 // These opcodes can be reduced to a single Assign as long as the [ref]s are the same
 internal sealed class AssignAndPushReferenceValue : IPeepholeOptimization {
     public ReadOnlySpan<DreamProcOpcode> GetOpcodes() {
         return [
-            DreamProcOpcode.AppendNoPush,
+            DreamProcOpcode.AssignNoPush,
             DreamProcOpcode.PushReferenceValue
         ];
     }

5516ed11f9bcd69c9111aef531b19f78479a6be5
ike709 <ike709@users.noreply.github.com>
2024-11-04 20:01:48 -0600

    Update DMCompiler/DM/Builders/DMASTFolder.cs

--- DMCompiler/DM/Builders/DMASTFolder.cs
+++ DMCompiler/DM/Builders/DMASTFolder.cs
@@ -222,10 +222,12 @@ public class DMASTFolder {
             case DMASTLeftShift leftShift: {
                 if (leftShift is { LHS: DMASTConstantInteger lhsInt, RHS: DMASTConstantInteger rhsInt }) {
                     return new DMASTConstantInteger(expression.Location, lhsInt.Value << rhsInt.Value);
+                }
                 break;
             case DMASTRightShift rightShift: {
                 if (rightShift is { LHS: DMASTConstantInteger lhsInt, RHS: DMASTConstantInteger rhsInt }) {
                     return new DMASTConstantInteger(expression.Location, lhsInt.Value >> rhsInt.Value);
+                }
                 break;
             }
             case DMASTBinaryAnd binaryAnd: {

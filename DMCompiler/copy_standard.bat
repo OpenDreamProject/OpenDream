0bad2349019ef0303bea2498910a767613cd396f
Furior <furiorg@gmail.com>
2025-02-12 21:00:22 +0700

    nuke useless files

--- DMCompiler/copy_standard.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if not exist bin\Debug\net9.0\DMStandard mkdir bin\Debug\net9.0\DMStandard
-xcopy DMStandard bin\Debug\net9.0\DMStandard /y /s /e
-if not exist bin\Release\net9.0\DMStandard mkdir bin\Release\net9.0\DMStandard
-xcopy DMStandard bin\Release\net9.0\DMStandard /y /s /e

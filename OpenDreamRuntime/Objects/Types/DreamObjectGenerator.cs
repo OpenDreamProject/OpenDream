82b6b866217878764ae0f7b1c881884fceb4d91c
amy <amylizzlep@gmail.com>
2025-02-18 23:09:46 +0000

    generator

---
+++ OpenDreamRuntime/Objects/Types/DreamObjectGenerator.cs
@@ -0,0 +1,40 @@
+using OpenDreamRuntime.Procs;
+
+namespace OpenDreamRuntime.Objects.Types;
+
+public sealed class DreamObjectGenerator : DreamObject {
+
+    public DreamValue A { get; private set; }
+    public DreamValue B { get; private set; }
+    public GeneratorOutputType OutputType { get; private set; }
+    public GeneratorDistribution Distribution { get; private set; }
+
+
+    public DreamObjectGenerator(DreamObjectDefinition objectDefinition, DreamValue A, DreamValue B, GeneratorOutputType outputType, GeneratorDistribution dist) : base(objectDefinition) {
+        this.A = A;
+        this.B = B;
+        this.OutputType = outputType;
+        this.Distribution = dist;
+    }
+    public override void Initialize(DreamProcArguments args) {
+        base.Initialize(args);
+    }
+}
+
+public enum GeneratorOutputType {
+    Num,
+    Vector,
+    Box,
+    Color,
+    Circle,
+    Sphere,
+    Square,
+    Cube
+}
+
+public enum GeneratorDistribution {
+    Uniform,
+    Normal,
+    Linear,
+    Square
+}

519db1ed9d605498697ba6add3c6787a4c0e30c4
Ruzihm <ruzihm.code@gmail.com>
2025-03-16 13:45:17 -0400

    cleanup helper scripts

--- byondapi/build-byondtest.bat
+++ byondapi/build-byondtest.bat
@@ -1,3 +1,7 @@
-git -C ../meowtonin reset --hard
-cargo build --target=i686-pc-windows-msvc
-copy .\target\i686-pc-windows-msvc\debug\byondapitest.dll ..\TestGame\byondapitest.dll
+rem convenience script for building byondapi & test in windows targeting byond
+rem run this after intiializing git submodules
+rem Useful for parity testing
+
+git -C "%~dp0\..\meowtonin" reset --hard
+cargo build --manifest-path "%~dp0\Cargo.toml" --target=i686-pc-windows-msvc
+copy "%~dp0\target\i686-pc-windows-msvc\debug\byondapitest.dll" "%~dp0\..\TestGame\byondapitest.dll"
--- byondapi/build.bat
+++ byondapi/build.bat
@@ -1,21 +1,24 @@
-rem TODO: make this a real script
-cargo clean -p trampoline
-cargo clean -p byondapitest
-del ..\meowtonin\crates\sys\link\windows\byondapi.lib
-del ..\bin\Content.Server\byondcore.dll
-del ..\bin\Content.Tests\byondcore.dll
-del ..\bin\Content.IntegrationTests\byondcore.dll
-del ..\bin\Content.Server\byondapitest.dll
-del ..\bin\Content.Tests\byondapitest.dll
-del ..\bin\Content.IntegrationTests\byondapitest.dll
+rem convenience script for building byondapi in windows targeting opendream
+rem run this after initializing git submodules and building with dotnet/visual studio
+rem TODO: use release instead of debug
 
-cargo build -p trampoline
-copy .\target\debug\byondcore.dll.lib ..\meowtonin\crates\sys\link\windows\byondapi.lib
-copy .\target\debug\byondcore.dll  ..\bin\Content.Server\byondcore.dll
-copy .\target\debug\byondcore.dll  ..\bin\Content.Tests\byondcore.dll
-copy .\target\debug\byondcore.dll  ..\bin\Content.IntegrationTests\byondcore.dll
+cargo clean --manifest-path "%~dp0\Cargo.toml" -p trampoline
+cargo clean --manifest-path "%~dp0\Cargo.toml" -p byondapitest
+del "%~dp0\..\meowtonin\crates\sys\link\windows\byondapi.lib"
+del "%~dp0\..\bin\Content.Server\byondcore.dll"
+del "%~dp0\..\bin\Content.Tests\byondcore.dll"
+del "%~dp0\..\bin\Content.IntegrationTests\byondcore.dll"
+del "%~dp0\..\bin\Content.Server\byondapitest.dll"
+del "%~dp0\..\bin\Content.Tests\byondapitest.dll"
+del "%~dp0\..\bin\Content.IntegrationTests\byondapitest.dll"
 
-cargo build -p byondapitest
-copy .\target\debug\byondapitest.dll ..\bin\Content.Server\byondapitest.dll
-copy .\target\debug\byondapitest.dll ..\bin\Content.Tests\byondapitest.dll
-copy .\target\debug\byondapitest.dll  ..\bin\Content.IntegrationTests\byondapitest.dll
+cargo build --manifest-path "%~dp0\Cargo.toml" -p trampoline
+copy "%~dp0\target\debug\byondcore.dll.lib" "%~dp0\..\meowtonin\crates\sys\link\windows\byondapi.lib"
+copy "%~dp0\target\debug\byondcore.dll"  "%~dp0\..\bin\Content.Server\byondcore.dll"
+copy "%~dp0\target\debug\byondcore.dll"  "%~dp0\..\bin\Content.Tests\byondcore.dll"
+copy "%~dp0\target\debug\byondcore.dll"  "%~dp0\..\bin\Content.IntegrationTests\byondcore.dll"
+
+cargo build --manifest-path "%~dp0\Cargo.toml" -p byondapitest
+copy "%~dp0\target\debug\byondapitest.dll" "%~dp0\..\bin\Content.Server\byondapitest.dll"
+copy "%~dp0\target\debug\byondapitest.dll" "%~dp0\..\bin\Content.Tests\byondapitest.dll"
+copy "%~dp0\target\debug\byondapitest.dll" "%~dp0\..\bin\Content.IntegrationTests\byondapitest.dll"

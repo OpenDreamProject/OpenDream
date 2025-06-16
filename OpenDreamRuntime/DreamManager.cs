8c05c5b6c8feddee14a3d85beef119f8bb66a92b
amylizzle <amylizzle@users.noreply.github.com>
2025-06-16 16:27:43 +0100

    fixyfix

--- OpenDreamRuntime/DreamManager.cs
+++ OpenDreamRuntime/DreamManager.cs
@@ -368,7 +368,9 @@ public sealed partial class DreamManager {
                     ? new DreamValue(appearance.ToMutable())
                     : DreamValue.Null;
             case RefType.Proc:
-                return new(_objectTree.Procs[refId]);
+                return _objectTree.Procs.Count > refId
+                    ? new DreamValue(_objectTree.Procs[refId])
+                    : DreamValue.Null;
             case RefType.Number: // For the oh so few numbers this works with (most numbers clobber the ref type)
                 return new(BitConverter.Int32BitsToSingle(refId));
             default:

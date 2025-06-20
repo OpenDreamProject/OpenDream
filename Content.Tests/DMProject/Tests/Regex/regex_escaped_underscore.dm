ce0301685fa3ef7b8942620535d2224cfa11230f
amy <amylizzlep@gmail.com>
2025-06-20 23:58:11 +0100

    oops

--- Content.Tests/DMProject/Tests/Regex/regex_escaped_underscore.dm
+++ Content.Tests/DMProject/Tests/Regex/regex_escaped_underscore.dm
@@ -1,4 +1,3 @@
 /proc/RunTest()
 	var/regex/R = regex("\\_", "g")
-	CRASH(R.Replace_char("t_e_s_t","0"))
-	ASSERT(R.Replace_char("t_e_s_t","0") == "t0e0s0t")
+	ASSERT(R.Replace("t_e_s_t","0") == "t0e0s0t")
--- OpenDreamRuntime/Objects/Types/DreamObjectRegex.cs
+++ OpenDreamRuntime/Objects/Types/DreamObjectRegex.cs
@@ -56,7 +56,6 @@ public sealed class DreamObjectRegex(DreamObjectDefinition objectDefinition) : D
                                 newPatternBuilder.Append(']');
                         } else if (c == '_') {
                             newPatternBuilder.Append('_'); //I don't know why BYOND supports escaping this, but C# doesn't
-                            continue;
                         } else {
                             newPatternBuilder.Append('\\');
                             goto default;

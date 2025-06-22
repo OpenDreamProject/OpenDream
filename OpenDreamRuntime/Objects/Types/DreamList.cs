c2942dc84e9385784c2feb9ab00a0c9227167764
amy <amylizzlep@gmail.com>
2025-06-22 16:15:45 +0100

    throw

--- OpenDreamRuntime/Objects/Types/DreamList.cs
+++ OpenDreamRuntime/Objects/Types/DreamList.cs
@@ -217,6 +217,10 @@ public class DreamList : DreamObject, IDreamList {
                 AddValue(DreamValue.Null);
             }
         } else {
+            if (size < 0) {
+                //TODO emit configurable warning here
+                throw new InvalidOperationException("Setting a list size to a negative value is invalid");
+            }
             Cut(size + 1);
         }
 

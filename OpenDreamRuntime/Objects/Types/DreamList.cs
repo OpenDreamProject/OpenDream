e46f0fd206f807e0ec3ba714f831bf31180a0fe3
wixoa <wixoag@gmail.com>
2025-06-22 22:22:30 -0400

    Update OpenDreamRuntime/Objects/Types/DreamList.cs

--- OpenDreamRuntime/Objects/Types/DreamList.cs
+++ OpenDreamRuntime/Objects/Types/DreamList.cs
@@ -221,6 +221,7 @@ public class DreamList : DreamObject, IDreamList {
                 //TODO emit configurable warning here
                 throw new InvalidOperationException("Setting a list size to a negative value is invalid");
             }
+            
             Cut(size + 1);
         }
 

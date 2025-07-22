cb7c9b107be9a8d17f00e88dee5239673f35fb86
wixoa <wixoag@gmail.com>
2025-07-21 21:45:24 -0400

    Add `caller` and `callee` support to the disassembler (#2348)

--- OpenDreamRuntime/Procs/ProcDecoder.cs
+++ OpenDreamRuntime/Procs/ProcDecoder.cs
@@ -59,6 +59,8 @@ public struct ProcDecoder(IReadOnlyList<string> strings, byte[] bytecode) {
             case DMReference.Type.World: return DMReference.World;
             case DMReference.Type.SuperProc: return DMReference.SuperProc;
             case DMReference.Type.ListIndex: return DMReference.ListIndex;
+            case DMReference.Type.Caller: return DMReference.Caller;
+            case DMReference.Type.Callee: return DMReference.Callee;
             default: throw new Exception($"Invalid reference type {refType}");
         }
     }

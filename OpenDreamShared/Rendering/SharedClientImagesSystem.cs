023982174b44076d85026bad021e568038b2bdbc
wixoa <wixoag@gmail.com>
2025-08-10 00:25:16 -0400

    Update RT to v266.0.0 (#2354)

--- OpenDreamShared/Rendering/SharedClientImagesSystem.cs
+++ OpenDreamShared/Rendering/SharedClientImagesSystem.cs
@@ -1,36 +1,26 @@
 ﻿using Robust.Shared.Analyzers;
 using Robust.Shared.GameObjects;
-using Robust.Shared.Maths;
 using Robust.Shared.Serialization;
 using System;
-
+using System.Numerics;
 
 namespace OpenDreamShared.Rendering;
+
 [Virtual]
 public class SharedClientImagesSystem : EntitySystem {
     [Serializable, NetSerializable]
-    public sealed class AddClientImageEvent : EntityEventArgs {
-        public Vector3 TurfCoords;
-        public NetEntity AttachedEntity; //if this is NetEntity.Invalid (ie, a turf) use the TurfCoords instead
-        public NetEntity ImageEntity;
-
-        public AddClientImageEvent(NetEntity attachedEntity, Vector3 turfCoords, NetEntity imageEntity) {
-            TurfCoords = turfCoords;
-            ImageEntity = imageEntity;
-            AttachedEntity = attachedEntity;
-        }
+    public sealed class AddClientImageEvent(NetEntity attachedEntity, Vector3 turfCoords, NetEntity imageEntity)
+        : EntityEventArgs {
+        public Vector3 TurfCoords = turfCoords;
+        public NetEntity AttachedEntity = attachedEntity; //if this is NetEntity.Invalid (ie, a turf) use the TurfCoords instead
+        public NetEntity ImageEntity = imageEntity;
     }
 
     [Serializable, NetSerializable]
-    public sealed class RemoveClientImageEvent : EntityEventArgs {
-        public Vector3 TurfCoords;
-        public NetEntity AttachedEntity; //if this is NetEntity.Invalid (ie, a turf) use the TurfCoords instead
-        public NetEntity ImageEntity;
-
-        public RemoveClientImageEvent(NetEntity attachedEntity, Vector3 turfCoords, NetEntity imageEntity) {
-            TurfCoords = turfCoords;
-            ImageEntity = imageEntity;
-            AttachedEntity = attachedEntity;
-        }
+    public sealed class RemoveClientImageEvent(NetEntity attachedEntity, Vector3 turfCoords, NetEntity imageEntity)
+        : EntityEventArgs {
+        public Vector3 TurfCoords = turfCoords;
+        public NetEntity AttachedEntity = attachedEntity; //if this is NetEntity.Invalid (ie, a turf) use the TurfCoords instead
+        public NetEntity ImageEntity = imageEntity;
     }
 }

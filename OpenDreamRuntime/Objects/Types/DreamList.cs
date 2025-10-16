859398e6e0f9182c2e26ec6f88390833150c479f
Redmoogle <redmoogle@rednako.xyz>
2025-10-16 17:54:50 -0400

    Optimize FIndValue for values that don't exist

--- OpenDreamRuntime/Objects/Types/DreamList.cs
+++ OpenDreamRuntime/Objects/Types/DreamList.cs
@@ -261,6 +261,8 @@ public class DreamList : DreamObject, IDreamList {
     public virtual int FindValue(DreamValue value, int start = 1, int end = 0) {
         if (end == 0 || end > _values.Count) end = _values.Count + 1;
 
+        if(!ContainsValue(value)) return 0;
+        
         for (int i = start; i < end; i++) {
             if (_values[i - 1].Equals(value)) return i;
         }

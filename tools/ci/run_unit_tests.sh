cf291d0452109db1f987401c25c4bc5c155ee5d9
amy <amylizzlep@gmail.com>
2025-10-24 21:27:41 +0100

    lets see how we go

---
+++ tools/ci/run_unit_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+touch errors.log
+
+find "$SEARCH_DIR" -type f -name "*.dm" | while read -r file; do
+	echo "Running dm.sh on $file"
+	"dm.sh" Content.Tests/DMProject/environment.dme -DBYOND_UNIT_TEST="$file" 
+	DreamDaemon Content.Tests/DMProject/environment.dmb -once -close -trusted -verbose -invisible
+done
+
+if [ -s "errors.log" ]
+then
+	echo "Errors detected!"
+	sed -i '/^[[:space:]]*$/d' ./errors.log
+	cat errors.log
+	exit 1
+else
+	echo "No errors detected."
+	exit 0
+fi

032a684caeb0669f7c80642ec66ec43eb566b134
amy <amylizzlep@gmail.com>
2025-10-24 21:37:34 +0100

    I wish running this as a local action worked

--- tools/ci/run_unit_tests.sh
+++ tools/ci/run_unit_tests.sh
@@ -5,7 +5,8 @@ touch errors.log
 
 find Content.Tests/DMProject/Tests -type f -name "*.dm" | while read -r file; do
 	echo "Running dm.sh on $file"
-	"dm.sh" Content.Tests/DMProject/environment.dme -DBYOND_UNIT_TEST="$file" 
+	tools/ci/dm.sh Content.Tests/DMProject/environment.dme -DBYOND_UNIT_TEST="$file" 
+	echo "Running $file"
 	DreamDaemon Content.Tests/DMProject/environment.dmb -once -close -trusted -verbose -invisible
 done
 

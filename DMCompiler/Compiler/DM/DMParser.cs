fe8a440f9512b297380943aa36fcbf05dc0d9398
ike709 <ike709@github.com>
2026-07-27 04:51:31 -0500

    `image()` requires at least 1 arg

--- DMCompiler/Compiler/DM/DMParser.cs
+++ DMCompiler/Compiler/DM/DMParser.cs
@@ -2755,6 +2755,14 @@ namespace DMCompiler.Compiler.DM {
 
                         return new DMASTRgb(identifier.Location, callParameters);
                     }
+                    case "image": {
+                        if (callParameters.Length < 1)
+                            Emit(WarningCode.InvalidArgumentCount, callLoc,
+                                "Expected at least 1 argument for image()");
+
+                        return new DMASTProcCall(callLoc,
+                            new DMASTCallableProcIdentifier(callLoc, identifier.Identifier), callParameters);
+                    }
                     case "animate": {
                         if (callParameters.Length is < 1)
                             Emit(WarningCode.InvalidArgumentCount, callLoc,

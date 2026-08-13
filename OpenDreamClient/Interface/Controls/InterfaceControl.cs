8e6ee8409bc9e28a985fad66ccaf12bca855fbe6
wixoa <wixoag@gmail.com>
2026-08-13 03:54:35 -0400

    Ignore the IDs on MAIN elements (#2699)

--- OpenDreamClient/Interface/Controls/InterfaceControl.cs
+++ OpenDreamClient/Interface/Controls/InterfaceControl.cs
@@ -42,7 +42,7 @@ public abstract class InterfaceControl : InterfaceElement {
     protected abstract Control CreateUIElement();
 
     protected override void UpdateElementDescriptor() {
-        UIElement.Name = ControlDescriptor.Name.Value;
+        UIElement.Name = ControlDescriptor.Id.Value;
 
         //transparent is default because it's white with 0 alpha, and DMF color can't have none-255 alpha
         StyleBox? styleBox = (ControlDescriptor.BackgroundColor.Value != Color.Transparent)
